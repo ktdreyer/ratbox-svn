efd36720aa6d72c6dc34c454ba0d5db33cd33efb
tomh <tomh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
1999-08-03 06:18:25 +0000

    missed a log thing in m_kill somehow
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@65 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/m_kill.c
+++ src/m_kill.c
@@ -26,6 +26,7 @@
 #include "client.h"
 #include "ircd.h"
 #include "numeric.h"
+#include "s_log.h"
 #include "s_serv.h"
 #include "send.h"
 #include "whowas.h"
@@ -215,10 +216,10 @@ int m_kill(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
                      "Received KILL message for %s. From %s Path: %s!%s",
                      acptr->name, parv[0], inpath, path);
 
-#if defined(USE_SYSLOG) && defined(SYSLOG_KILL)
+#if defined(SYSLOG_KILL)
   if (IsOper(sptr))
-    syslog(LOG_INFO,"KILL From %s For %s Path %s!%s",
-                        parv[0], acptr->name, inpath, path);
+    log(L_INFO,"KILL From %s For %s Path %s!%s",
+        parv[0], acptr->name, inpath, path);
 #endif
   /*
   ** And pass on the message to other servers. Note, that if KILL
