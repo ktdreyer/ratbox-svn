6750b016d04a3fe345dbcc7146eba6109d6590ea
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
1999-08-05 03:40:20 +0000

    - fixed broken m_unkline.c it would unkline everything. duh
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@77 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/m_unkline.c
+++ src/m_unkline.c
@@ -183,7 +183,6 @@ K:bar:No reason (1997/08/30 14.56):foo
           char *found_comment;
 
           strncpy_irc(buff, buf, BUFSIZE);      /* extra paranoia */
-          buff[BUFSIZE] = '\0';
 
           p = strchr(buff,'\n');
           if(p)
@@ -241,7 +240,6 @@ K:bar:No reason (1997/08/30 14.56):foo
           char *found_host;
 
           strncpy_irc(buff, buf, BUFSIZE);
-          buff[BUFSIZE] = '\0';
 /*
 #Dianora!db@ts2-11.ottawa.net K'd: foo@bar:No reason
 K:bar:No reason (1997/08/30 14.56):foo
