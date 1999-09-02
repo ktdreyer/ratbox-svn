52855c88fec51356701e1b9d151ceadbba2fdf0b
wnder <wnder@b93f080a-4cfa-0310-a19a-80a68f531ef9>
1999-09-02 01:23:51 +0000

    goes with ircdauth.c
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@280 b93f080a-4cfa-0310-a19a-80a68f531ef9

---
+++ include/ircdauth.h
@@ -0,0 +1,45 @@
+/************************************************************************
+ *
+ *   IRC - Internet Relay Chat, include/ircdauth.h
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+
+#ifndef INCLUDED_ircdauth_h
+#define INCLUDED_ircdauth_h
+
+struct Client;
+
+/* ircdauth.c prototypes */
+
+int ConnectToIAuth();
+int GenerateClientID(const struct Client *cptr);
+int ParseIAuth();
+
+typedef struct IrcdAuthentication IrcdAuth;
+
+#define  NOSOCK        (-1)
+#define  MAXPARAMS     15
+
+struct IrcdAuthentication
+{
+	char hostname[HOSTLEN + 1]; /* hostname of IAuth server */
+	int port; /* port for connection */
+	int socket; /* socket descriptor for IAuth connection */
+};
+
+#endif /* INCLUDED_ircdauth_h */
