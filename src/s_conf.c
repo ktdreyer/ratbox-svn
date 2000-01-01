01b8b32f94d0b37765f95745e21665d15289805d
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2000-01-01 01:01:39 +0000

    - missing NULL assignment would cause double free's and eventual   core on malformed U: lines.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@546 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/s_conf.c
+++ src/s_conf.c
@@ -2392,6 +2392,7 @@ static void initconf(FBFILE* file, int use_include)
         {
           dontadd = 1;
           MyFree(aconf->user);
+          aconf->user = (char *)NULL;
           aconf->name = aconf->host;
           aconf->host = (char *)NULL;
           aconf->next = u_conf;
