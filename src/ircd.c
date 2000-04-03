022f0c70ac378297b66ad87460f9b67ce1ee9046
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2000-04-03 03:37:02 +0000

    - MAX_SETBACK_TIME added
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@919 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/ircd.c
+++ src/ircd.c
@@ -369,7 +369,7 @@ static time_t io_loop(time_t delay)
                  CurrentTime, lasttimeofday);
       report_error(to_send, me.name, 0);
     }
-  else if ((lasttimeofday + 1800) < CurrentTime)
+  else if ((lasttimeofday + MAX_SETBACK_TIME) < CurrentTime)
     {
       log(L_ERROR, "Clock Failure (%d)", errno);
       sendto_ops("Clock set back more than %d seconds, TS can be corrupted",
