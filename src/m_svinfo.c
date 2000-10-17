a0082cd8c61dd7bf2ecb06ee0d6bb838bcccd265
lusky <lusky@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2000-10-17 06:37:11 +0000

    fixed a bug tickled by sending a bogus SERVER command from hacked server auditted leakage of IP address from get_client_name calls
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@1073 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/m_svinfo.c
+++ src/m_svinfo.c
@@ -118,7 +118,7 @@ int m_svinfo(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
        * TS_ONLY we can't fall back to the non-TS protocol so
        * we drop the link  -orabidoo
        */
-      sendto_ops("Link %s dropped, wrong TS protocol version (%s,%s)",
+      sendto_realops("Link %s dropped, wrong TS protocol version (%s,%s)",
                  get_client_name(sptr, TRUE), parv[1], parv[2]);
       return exit_client(sptr, sptr, sptr, "Incompatible TS version");
     }
@@ -132,7 +132,7 @@ int m_svinfo(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
 
   if (deltat > ConfigFileEntry.ts_max_delta)
     {
-      sendto_ops(
+      sendto_realops(
        "Link %s dropped, excessive TS delta (my TS=%d, their TS=%d, delta=%d)",
                  get_client_name(sptr, TRUE), CurrentTime, theirtime,deltat);
       return exit_client(sptr, sptr, sptr, "Excessive TS delta");
