06f805d90c8d60606201575f50dd46fdd58ff751
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2000-11-05 07:27:25 +0000

    - redid m_oper.c to clean that up a bit - changed the header in m_challenge.c from m_version.c to m_challenge.c
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@1434 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/m_oper.c
+++ src/m_oper.c
@@ -25,6 +25,7 @@
 
 #include "handlers.h"
 #include "client.h"
+#include "common.h"
 #include "fdlist.h"
 #include "irc_string.h"
 #include "ircd.h"
@@ -100,6 +101,11 @@
 
 static void log_fname( struct Client *sptr, char *name );
 static struct ConfItem *find_password_aconf(char *name, struct Client *sptr);
+static int match_oper_password(char *password, struct ConfItem *aconf);
+static int oper_up( struct Client *sptr, struct ConfItem *aconf );
+#ifdef CRYPT_OPER_PASSWORD
+extern        char *crypt();
+#endif /* CRYPT_OPER_PASSWORD */
 
 /*
 ** m_oper
@@ -110,11 +116,8 @@ static struct ConfItem *find_password_aconf(char *name, struct Client *sptr);
 int m_oper(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
 {
   struct ConfItem *aconf;
-  char  *name, *password, *encr;
-#ifdef CRYPT_OPER_PASSWORD
-  extern        char *crypt();
-#endif /* CRYPT_OPER_PASSWORD */
-  char *operprivs;
+  char  *name;
+  char  *password;
 
   name = parc > 1 ? parv[1] : (char *)NULL;
   password = parc > 2 ? parv[2] : (char *)NULL;
@@ -131,98 +134,17 @@ int m_oper(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
   if(aconf == NULL)
     return 0;
 
-#ifdef CRYPT_OPER_PASSWORD
-  /* use first two chars of the password they send in as salt */
-
-  /* passwd may be NULL pointer. Head it off at the pass... */
-  if (password && *aconf->passwd)
-    encr = crypt(password, aconf->passwd);
-  else
-    encr = "";
-#else
-  encr = password;
-#endif  /* CRYPT_OPER_PASSWORD */
-
-  if ((aconf->status & CONF_OPS) &&
-      0 == strcmp(encr, aconf->passwd) && !attach_conf(sptr, aconf))
+  if ( match_oper_password(password,aconf) )
     {
-      int old = (sptr->umodes & ALL_UMODES);
-      
-      if (aconf->status == CONF_LOCOP)
-        {
-          SetLocOp(sptr);
-          if((int)aconf->hold)
-            {
-              sptr->umodes |= ((int)aconf->hold & ALL_UMODES); 
-              sendto_one(sptr, ":%s NOTICE %s :*** Oper flags set from conf",
-                         me.name,parv[0]);
-            }
-          else
-            {
-              sptr->umodes |= (LOCOP_UMODES);
-            }
-        }
-      else
-        {
-          SetOper(sptr);
-          if((int)aconf->hold)
-            {
-              sptr->umodes |= ((int)aconf->hold & ALL_UMODES); 
-              if( !IsSetOperN(sptr) )
-                sptr->umodes &= ~FLAGS_NCHANGE;
-              
-              sendto_one(sptr, ":%s NOTICE %s :*** Oper flags set from conf",
-                         me.name,parv[0]);
-            }
-          else
-            {
-              sptr->umodes |= (OPER_UMODES);
-            }
-        }
-      SetIPHidden(sptr);
-      Count.oper++;
+      (void)attach_conf(sptr, aconf);
+      (void)oper_up( sptr, aconf );
 
-      SetElined(cptr);
-      
-      /* LINKLIST */  
-      /* add to oper link list -Dianora */
-      cptr->next_oper_client = oper_cptr_list;
-      oper_cptr_list = cptr;
-
-      if(cptr->confs)
-        {
-          struct ConfItem *aconf;
-          aconf = cptr->confs->value.aconf;
-          operprivs = oper_privs_as_string(cptr,aconf->port);
-        }
-      else
-        operprivs = "";
-
-#ifdef CUSTOM_ERR
-      sendto_ops("%s (%s@%s) has just acquired the personality of a petty megalomaniacal tyrant [IRC(%c)p]", parv[0],
-#else
-      sendto_ops("%s (%s@%s) is now operator (%c)", parv[0],
-#endif /* CUSTOM_ERR */
-                 sptr->username, sptr->host,
-                 IsGlobalOper(sptr) ? 'O' : 'o');
-      send_umode_out(cptr, sptr, old);
-
-      sendto_one(sptr, form_str(RPL_YOUREOPER), me.name, parv[0]);
-      sendto_one(sptr, ":%s NOTICE %s :*** Oper privs are %s",me.name,parv[0],
-                 operprivs);
-      SendMessageFile(sptr, &ConfigFileEntry.opermotd);
-
-#if !defined(CRYPT_OPER_PASSWORD) && (defined(FNAME_OPERLOG) || defined(SYSLOG_OPER))
-        encr = "";
-#endif
-        log(L_TRACE, "OPER %s by %s!%s@%s",
-            name, parv[0], sptr->username, sptr->host);
-
-	log_fname(sptr, name);
+      log(L_TRACE, "OPER %s by %s!%s@%s",
+	  name, parv[0], sptr->username, sptr->host);
+      log_fname(sptr, name);
     }
   else
     {
-      detach_conf(sptr, aconf);
       sendto_one(sptr,form_str(ERR_PASSWDMISMATCH),me.name, parv[0]);
       if (ConfigFileEntry.failed_oper_notice)
 	{
@@ -334,3 +256,129 @@ static struct ConfItem *find_password_aconf(char *name, struct Client *sptr)
     }
   return(aconf);
 }
+
+/*
+ * match_oper_password
+ *
+ */
+
+static int match_oper_password(char *password, struct ConfItem *aconf)
+{
+  char *encr;
+
+  if (!aconf->status & CONF_OPS)
+    return NO;
+
+#ifdef CRYPT_OPER_PASSWORD
+  /* use first two chars of the password they send in as salt */
+
+  /* passwd may be NULL pointer. Head it off at the pass... */
+  if (password && *aconf->passwd)
+    encr = crypt(password, aconf->passwd);
+  else
+    encr = "";
+#else
+  encr = password;
+#endif  /* CRYPT_OPER_PASSWORD */
+
+  if( strcmp(encr, aconf->passwd) == 0 )
+    return YES;
+  else
+    return NO;
+}
+
+/*
+ * oper_up
+ *
+ * inputs	- pointer to given client to oper
+ *		- pointer to ConfItem to use
+ * output	- none
+ * side effects	-
+ * Blindly opers up given sptr, using aconf info
+ * all checks on passwords have already been done.
+ * This could also be used by rsa oper routines. 
+ */
+
+static int oper_up( struct Client *sptr, struct ConfItem *aconf )
+{
+  int old = (sptr->umodes & ALL_UMODES);
+  char *operprivs;
+
+  if (aconf->status == CONF_LOCOP)
+    {
+      SetLocOp(sptr);
+      if((int)aconf->hold)
+	{
+	  sptr->umodes |= ((int)aconf->hold & ALL_UMODES); 
+	  sendto_one(sptr, ":%s NOTICE %s :*** Oper flags set from conf",
+		     me.name,sptr->name);
+	}
+      else
+	{
+	  sptr->umodes |= (LOCOP_UMODES);
+	}
+    }
+  else
+    {
+      SetOper(sptr);
+      if((int)aconf->hold)
+	{
+	  sptr->umodes |= ((int)aconf->hold & ALL_UMODES); 
+	  if( !IsSetOperN(sptr) )
+	    sptr->umodes &= ~FLAGS_NCHANGE;
+	  
+	  sendto_one(sptr, ":%s NOTICE %s :*** Oper flags set from conf",
+		     me.name,sptr->name);
+	}
+      else
+	{
+	  sptr->umodes |= (OPER_UMODES);
+	}
+    }
+  SetIPHidden(sptr);
+  Count.oper++;
+
+  SetElined(sptr);
+      
+  /* LINKLIST */  
+  /* add to oper link list -Dianora */
+  sptr->next_oper_client = oper_cptr_list;
+  oper_cptr_list = sptr;
+
+  if(sptr->confs)
+    {
+      struct ConfItem *aconf;
+      aconf = sptr->confs->value.aconf;
+      operprivs = oper_privs_as_string(sptr,aconf->port);
+    }
+  else
+    operprivs = "";
+
+#ifdef CUSTOM_ERR
+  sendto_ops("%s (%s@%s) has just acquired the personality of a petty megalomaniacal tyrant [IRC(%c)p]", sptr->name,
+	     sptr->username, sptr->host,
+	     IsGlobalOper(sptr) ? 'O' : 'o');
+#else
+  sendto_ops("%s (%s@%s) is now operator (%c)", sptr->name,
+	     sptr->username, sptr->host,
+	     IsGlobalOper(sptr) ? 'O' : 'o');
+#endif /* CUSTOM_ERR */
+
+  send_umode_out(sptr, sptr, old);
+
+  sendto_one(sptr, form_str(RPL_YOUREOPER), me.name, sptr->name);
+  sendto_one(sptr, ":%s NOTICE %s :*** Oper privs are %s",me.name,
+	     sptr->name,
+	     operprivs);
+  SendMessageFile(sptr, &ConfigFileEntry.opermotd);
+
+  return 1;
+}
+
+
+
+
+
+
+
+
