77524061ecd48c87c752b0cc20a105ef3f0b3d2f
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2000-12-05 21:31:03 +0000

    - redid m_set.c precalculate newval etc.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@2124 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_set.c
+++ modules/m_set.c
@@ -65,40 +65,43 @@ struct SetStruct
 {
   char  *name;
   int   (*handler)();
-  int   _unused1;	/* unused at this time */
-  int   _unused2;	/* unused at this time */
+  int   expects_two_args; /* 1 if it expects char *arg,int,
+			 * 0 if it expects only int
+			 */
 };
 
 
-int quote_autoconn(struct Client *, int, char **);
-int quote_floodcount(struct Client *, int, char **);
-int quote_floodtime(struct Client *, int, char **);
-int quote_idletime(struct Client *, int, char **);
-int quote_log(struct Client *, int, char **);
-int quote_max(struct Client *, int, char **);
-int quote_spamnum(struct Client *, int, char **);
-int quote_spamtime(struct Client *, int, char **);
-int quote_shide(struct Client *, int, char **);
-int quote_chide(struct Client *, int, char **);
+int quote_autoconn(struct Client *, char *, int);
+int quote_autoconnall(struct Client *, int);
+int quote_floodcount(struct Client *, int);
+int quote_floodtime(struct Client *, int);
+int quote_idletime(struct Client *, int);
+int quote_log(struct Client *, int);
+int quote_max(struct Client *, int);
+int quote_spamnum(struct Client *, int);
+int quote_spamtime(struct Client *, int);
+int quote_shide(struct Client *, int);
+int quote_chide(struct Client *, int);
 int list_quote_commands(struct Client *);
 
 
 static struct SetStruct set_cmd_table[] =
 {
-  /* name		function		unused	unused */
-  /* --------------------------------------------------------- */
-  { "AUTOCONN",		quote_autoconn,		0,	3 },
-  { "FLOODCOUNT",	quote_floodcount,	0,	2 },
-  { "FLOODTIME",	quote_floodtime,	0,	2 },
-  { "IDLETIME",		quote_idletime,		0,	2 },
-  { "LOG",		quote_log,		0,	2 },
-  { "MAX",		quote_max,		0,	2 },
-  { "SPAMNUM",		quote_spamnum,		0,	2 },
-  { "SPAMTIME",		quote_spamtime,		0,	2 },
-  { "SHIDE",		quote_shide,		0,	2 },
-  { "CHIDE",		quote_chide,		0,	2 },
-  /* --------------------------------------------------------- */
-  { (char *) 0,		(int (*)()) 0,		0,	0 }
+  /* name		function	expects two args */
+  /* --------------------------------------------------- */
+  { "AUTOCONN",		quote_autoconn,		1 },
+  { "AUTOCONNALL",	quote_autoconnall,	0 },
+  { "FLOODCOUNT",	quote_floodcount,	0 },
+  { "FLOODTIME",	quote_floodtime,	0 },
+  { "IDLETIME",		quote_idletime,		0 },
+  { "LOG",		quote_log,		0 },
+  { "MAX",		quote_max,		0 },
+  { "SPAMNUM",		quote_spamnum,		0 },
+  { "SPAMTIME",		quote_spamtime,		0 },
+  { "SHIDE",		quote_shide,		0 },
+  { "CHIDE",		quote_chide,		0 },
+  /* ---------------------------------------------------- */
+  { (char *) 0,		(int (*)()) 0,		0 }
 };
 
 
@@ -123,141 +126,112 @@ int list_quote_commands(struct Client *sptr)
   return(0);
 }
 
-
-
 /* SET AUTOCONN */
-int quote_autoconn( struct Client *sptr,
-		    int parc, char *parv[])
+int quote_autoconn( struct Client *sptr, char *arg, int newval)
 {
-  if (parc > 3)
-  {
-    int newval = atoi(parv[3]);
+  set_autoconn(sptr, sptr->name, arg, newval);
+  return(0);
+}
 
-    if (!irccmp(parv[2],"ALL"))
+/* SET AUTOCONNALL */
+int quote_autoconnall( struct Client *sptr, int newval)
+{
+  if(newval >= 0)
     {
-      sendto_realops("%s has changed AUTOCONN ALL to %i", parv[0], newval);
+      sendto_realops("%s has changed AUTOCONN ALL to %i",
+		     sptr->name, newval);
 
       GlobalSetOptions.autoconn = newval;
     }
-    else /* if (!irccmp(parv[2],"ALL")) */
+  else 
     {
-      set_autoconn(sptr, parv[0], parv[2], newval);
+      sendto_one(sptr, ":%s NOTICE %s :AUTOCONN ALL is currently %i",
+		 me.name, sptr->name, GlobalSetOptions.autoconn);
     }
-  }
-  else /* if (parc > 3) */
-  {
-    sendto_one(sptr, ":%s NOTICE %s :AUTOCONN ALL is currently %i",
-		me.name, parv[0], GlobalSetOptions.autoconn);
-  }
   return(0);
 }
 
 
 /* SET FLOODCOUNT */
-int quote_floodcount( struct Client *sptr,
-		      int parc, char *parv[])
+int quote_floodcount( struct Client *sptr, int newval)
 {
-  if (parc > 2)
-  {
-    int newval = atoi(parv[2]);
-
-    if (newval <= 0)
+  if(newval >= 0)
     {
-      sendto_one(sptr, ":%s NOTICE %s :FLOODCOUNT must be > 0",
-	me.name, parv[0]);
-
-      return(0);
+      GlobalSetOptions.floodcount = newval;
+      sendto_realops("%s has changed FLOODCOUNT to %i", sptr->name,
+		     GlobalSetOptions.floodcount);
     }
-
-    GlobalSetOptions.floodcount = newval;
-    sendto_realops("%s has changed FLOODCOUNT to %i", parv[0], 
-		   GlobalSetOptions.floodcount);
-  }
-  else /* if (parc > 2) */
-  {
-    sendto_one(sptr, ":%s NOTICE %s :FLOODCOUNT is currently %i",
-	me.name, parv[0], GlobalSetOptions.floodcount);
-  }
+  else
+    {
+      sendto_one(sptr, ":%s NOTICE %s :FLOODCOUNT is currently %i",
+		 me.name, sptr->name, GlobalSetOptions.floodcount);
+    }
   return(0);
 }
 
 
 /* SET FLOODTIME */
-int quote_floodtime( struct Client *sptr,
-		     int parc, char *parv[])
+int quote_floodtime( struct Client *sptr, int newval )
 {
-  if (parc > 2)
+
+  if(newval >= 0)
     {
-      int newval = atoi(parv[2]);
-
-      if (newval < 0)
-	{
-	  sendto_one(sptr, ":%s NOTICE %s :FLOODTIME must be > 0",
-		     me.name, parv[0]);
-
-	  return(0);
-	}
       GlobalSetOptions.floodtime = newval;
 
       if (GlobalSetOptions.floodtime == 0)
 	{
-	  sendto_realops("%s has disabled the ANTI_FLOOD code", parv[0]);
+	  sendto_realops("%s has disabled the ANTI_FLOOD code", sptr->name);
 	}
       else
 	{
-	  sendto_realops("%s has changed FLOODTIME to %i", parv[0], 
+	  sendto_realops("%s has changed FLOODTIME to %i", sptr->name, 
 			 GlobalSetOptions.floodtime);
 	}
     }
-  else /* if (parc > 2) */
+  else
     {
       sendto_one(sptr, ":%s NOTICE %s :FLOODTIME is currently %i",
-		 me.name, parv[0], GlobalSetOptions.floodtime);
+		 me.name, sptr->name, GlobalSetOptions.floodtime);
     }
   return(0);
 }
 
 /* SET IDLETIME */
-int quote_idletime( struct Client *sptr,
-		    int parc, char *parv[])
+int quote_idletime( struct Client *sptr, int newval )
 {
-  if (parc > 2)
+  if(newval >= 0)
     {
-      int newval = atoi(parv[2]);
-
       if (newval == 0)
 	{
-	  sendto_realops("%s has disabled IDLE_CHECK", parv[0]);
+	  sendto_realops("%s has disabled IDLE_CHECK", sptr->name);
 	  GlobalSetOptions.idletime = 0;
 	}
-      else /* if (newval == 0) */
+      else 
 	{
-	  sendto_realops("%s has changed IDLETIME to %i", parv[0], newval);
+	  sendto_realops("%s has changed IDLETIME to %i", sptr->name, newval);
 	  GlobalSetOptions.idletime = (newval*60);
 	}
     }
-  else /* if (parc > 2) */
+  else 
     {
       sendto_one(sptr, ":%s NOTICE %s :IDLETIME is currently %i",
-		 me.name, parv[0], GlobalSetOptions.idletime/60);
+		 me.name, sptr->name, GlobalSetOptions.idletime/60);
     }
   return(0);
 }
 
 
 /* SET LOG */
-int quote_log( struct Client *sptr,
-	       int parc, char *parv[])
+int quote_log( struct Client *sptr, int newval )
 {
-  if (parc > 2)
-    {
-      int newval = atoi(parv[2]);
-      const char *log_level_as_string;
+  const char *log_level_as_string;
 
+  if (newval >= 0)
+    {
       if (newval < L_WARN)
 	{
 	  sendto_one(sptr, ":%s NOTICE %s :LOG must be > %d (L_WARN)",
-		     me.name, parv[0], L_WARN);
+		     me.name, sptr->name, L_WARN);
 	  return(0);
 	}
 
@@ -269,75 +243,65 @@ int quote_log( struct Client *sptr,
       set_log_level(newval);
       log_level_as_string = get_log_level_as_string(newval);
       sendto_realops("%s has changed LOG level to %i (%s)",
-		     parv[0], newval, log_level_as_string);
+		     sptr->name, newval, log_level_as_string);
     }
-  else /* if (parc > 2) */
+  else 
     {
       sendto_one(sptr, ":%s NOTICE %s :LOG level is currently %i (%s)",
-		 me.name, parv[0], get_log_level(),
+		 me.name, sptr->name, get_log_level(),
 		 get_log_level_as_string(get_log_level()));
     }
   return(0);
 }
 
 /* SET MAX */
-int quote_max( struct Client *sptr, 
-	       int parc, char *parv[])
+int quote_max( struct Client *sptr, int newval )
 {
-  if (parc > 2)
+  if (newval > 0)
     {
-      int new_value = atoi(parv[2]);
-
-      if (new_value > MASTER_MAX)
+      if (newval > MASTER_MAX)
 	{
 	  sendto_one(sptr,
 	     ":%s NOTICE %s :You cannot set MAXCLIENTS to > MASTER_MAX (%d)",
-	     me.name, parv[0], MASTER_MAX);
+	     me.name, sptr->name, MASTER_MAX);
+	  return(0);
+	}
+
+      if (newval < 32)
+	{
+	  sendto_one(sptr,
+		     ":%s NOTICE %s :You cannot set MAXCLIENTS to < 32 (%d:%d)",
+		     me.name, sptr->name, GlobalSetOptions.maxclients, highest_fd);
 
 	  return(0);
 	}
 
-    if (new_value < 32)
-      {
-	sendto_one(sptr,
-		   ":%s NOTICE %s :You cannot set MAXCLIENTS to < 32 (%d:%d)",
-		   me.name, parv[0], GlobalSetOptions.maxclients, highest_fd);
+      GlobalSetOptions.maxclients = newval;
 
-	return(0);
-      }
+      sendto_realops("%s!%s@%s set new MAXCLIENTS to %d (%d current)",
+		     sptr->name, sptr->username, sptr->host,
+		     GlobalSetOptions.maxclients, Count.local);
 
-    GlobalSetOptions.maxclients = new_value;
-
-    sendto_realops("%s!%s@%s set new MAXCLIENTS to %d (%d current)",
-		   parv[0], sptr->username, sptr->host,
-		   GlobalSetOptions.maxclients, Count.local);
-
-    return(0);
+      return(0);
     }
-  sendto_one(sptr, ":%s NOTICE %s :Current Maxclients = %d (%d)",
-	     me.name, parv[0],
-	     GlobalSetOptions.maxclients, Count.local);
+  else
+    {
+      sendto_one(sptr, ":%s NOTICE %s :Current Maxclients = %d (%d)",
+		 me.name, sptr->name,
+		 GlobalSetOptions.maxclients, Count.local);
+    }
 
   return(0);
 }
 
 /* SET SPAMNUM */
-int quote_spamnum( struct Client *sptr,
-		   int parc, char *parv[])
+int quote_spamnum( struct Client *sptr, int newval )
 {
-  if (parc > 2)
+  if (newval > 0)
     {
-      int newval = atoi(parv[2]);
-
-      if (newval < 0)
-	{
-	  sendto_one(sptr, ":%s NOTICE %s :SPAMNUM must be > 0",
-		     me.name, parv[0]);
-	  return(0);
-	}
       if (newval == 0)
 	{
-	  sendto_realops("%s has disabled ANTI_SPAMBOT", parv[0]);
+	  sendto_realops("%s has disabled ANTI_SPAMBOT", sptr->name);
 	  GlobalSetOptions.spam_num = newval;
 	  return(0);
 	}
@@ -350,32 +314,22 @@ int quote_spamnum( struct Client *sptr,
 	  GlobalSetOptions.spam_num = newval;
 	}
       sendto_realops("%s has changed SPAMNUM to %i",
-		     parv[0], GlobalSetOptions.spam_num);
+		     sptr->name, GlobalSetOptions.spam_num);
     }
-  else /* if (parc > 2) */
+  else
     {
       sendto_one(sptr, ":%s NOTICE %s :SPAMNUM is currently %i",
 		 me.name,
-		 parv[0], GlobalSetOptions.spam_num);
+		 sptr->name, GlobalSetOptions.spam_num);
     }
   return(0);
 }
 
 /* SET SPAMTIME */
-int quote_spamtime( struct Client *sptr,
-		    int parc, char *parv[])
+int quote_spamtime( struct Client *sptr, int newval )
 {
-  if (parc > 2)
+  if (newval > 0)
     {
-      int newval = atoi(parv[2]);
-
-      if (newval <= 0)
-	{
-	  sendto_one(sptr, ":%s NOTICE %s :SPAMTIME must be > 0",
-		     me.name, parv[0]);
-
-	  return(0);
-	}
       if (newval < MIN_SPAM_TIME)
 	{
 	  GlobalSetOptions.spam_time = MIN_SPAM_TIME;
@@ -385,59 +339,53 @@ int quote_spamtime( struct Client *sptr,
 	  GlobalSetOptions.spam_time = newval;
 	}
       sendto_realops("%s has changed SPAMTIME to %i",
-		     parv[0], GlobalSetOptions.spam_time);
+		     sptr->name, GlobalSetOptions.spam_time);
     }
-  else /* if (parc > 2) */
+  else
     {
       sendto_one(sptr, ":%s NOTICE %s :SPAMTIME is currently %i",
-		 me.name, parv[0], GlobalSetOptions.spam_time);
+		 me.name, sptr->name, GlobalSetOptions.spam_time);
     }
   return(0);
 }
 
-int quote_chide( struct Client *sptr,
-		     int parc, char *parv[])
+int quote_chide( struct Client *sptr, int newval )
 {
-  if(parc > 2)
+  if(newval >= 0)
     {
-      int newval = atoi(parv[2]);
-
       if(newval)
 	GlobalSetOptions.hide_chanops = 1;
       else
 	GlobalSetOptions.hide_chanops = 0;
 
       sendto_realops("%s has changed CHIDE to %i",
-		     parv[0], GlobalSetOptions.hide_chanops);
+		     sptr->name, GlobalSetOptions.hide_chanops);
     }
   else
     {
       sendto_one(sptr, ":%s NOTICE %s :CHIDE is currently %i",
-		 me.name, parv[0], GlobalSetOptions.hide_chanops);
+		 me.name, sptr->name, GlobalSetOptions.hide_chanops);
 
     }
   return 0;
 }
 
-int quote_shide( struct Client *sptr,
-		     int parc, char *parv[])
+int quote_shide( struct Client *sptr, int newval )
 {
-  if(parc > 2)
+  if(newval >= 0)
     {
-      int newval = atoi(parv[2]);
-
       if(newval)
 	GlobalSetOptions.hide_server = 1;
       else
 	GlobalSetOptions.hide_server = 0;
 
       sendto_realops("%s has changed SHIDE to %i",
-		     parv[0], GlobalSetOptions.hide_server);
+		     sptr->name, GlobalSetOptions.hide_server);
     }
   else
     {
       sendto_one(sptr, ":%s NOTICE %s :SHIDE is currently %i",
-		 me.name, parv[0], GlobalSetOptions.hide_server);
+		 me.name, sptr->name, GlobalSetOptions.hide_server);
     }
   return 0;
 }
@@ -448,11 +396,13 @@ int quote_shide( struct Client *sptr,
  */
 int mo_set(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
 {
+  int newval;
+  int i;
+  char *arg;
+  char *intarg;
+
   if (parc > 1)
     {
-      int i;
-      int result;
-
       /*
        * Go through all the commands in set_cmd_table, until one is
        * matched.  I realize strcmp() is more intensive than a numeric
@@ -466,8 +416,47 @@ int mo_set(struct Client *cptr, struct Client *sptr, int parc, char *parv[])
 	      /*
 	       * Command found; now execute the code
 	       */
-	      result = set_cmd_table[i].handler( sptr, parc, parv);
-	      return(result);
+	      if (parc > 3)
+		{
+		  arg = parv[2];
+		  intarg = parv[3];
+		}
+	      else if(parc > 2)
+		{
+		  arg = NULL; /* not used */
+		  intarg = parv[2];
+		}
+
+	      if(parc > 2)
+		{
+		  newval = atoi(intarg);
+		  if(newval < 0)
+		    {
+		      sento_one(sptr,
+				":%s NOTICE %s :Value less than 0 illegal for %s",
+				me.name, sptr->name,
+				set_cmd_table[i].name);
+		      return 0;
+		    }
+		}
+	      else
+		newval = -1;
+
+	      if(set_cmd_table[i].expects_two_args)
+		{
+		  if(parc > 3)
+		    return(set_cmd_table[i].handler( sptr, arg, newval ));
+		  else
+		    {
+		      sendto_one(sptr,
+				 ":%s NOTICE %s :%s expects two args",
+				 me.name, sptr->name,
+				 set_cmd_table[i].name);
+		      return(0);
+		    }
+		}
+	      else
+		return(set_cmd_table[i].handler( sptr, newval ));
 	    }
 	}
       /*
