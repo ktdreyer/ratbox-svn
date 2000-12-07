a33395a367b7fdf05e73f3e244843abf82f2782e
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2000-12-07 00:52:29 +0000

    - redoing RPL_VCHANLIST so last %s is gone, since I'm building   up the channel sublist myself
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@2165 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/vchannel.c
+++ src/vchannel.c
@@ -175,7 +175,7 @@ void show_vchans(struct Client *cptr,
    /* include the root itself in the count */
    no_of_vchans = dlink_list_length(&chptr->vchan_list) + 1;
 
-   sendto_one(sptr, form_str(RPL_VCHANEXIST),
+   Sendto_one(sptr, form_str(RPL_VCHANEXIST),
               me.name, sptr->name, chptr->chname, no_of_vchans);
 
    vchan_show_ids(sptr, chptr);
