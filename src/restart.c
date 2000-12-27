119cb6f71b510070afcfa0e7436fa16cdb75c832
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2000-12-27 23:40:32 +0000

    - fixed the cold restart for now, really should have a warm restart as well
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@3261 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/restart.c
+++ src/restart.c
@@ -36,7 +36,7 @@ void restart(char *mesg)
 void server_reboot(void)
 {
   int i;
-  
+
   sendto_realops_flags(FLAGS_ALL,
 		       "Aieeeee!!!  Restarting server... memory: %d",
 		       get_maxrss());
@@ -48,9 +48,12 @@ void server_reboot(void)
    * when close handlers come into existance, comm_close() will be called
    * below, and the data flushing will be implicit.
    *    -- adrian
+   *
+   * bah, for now, the program ain't coming back to here, so forcibly
+   * close everything the "wrong" way for now, and just LEAVE...
    */
   for (i = 0; i < MAXCONNECTIONS; ++i)
-    fd_close(i);
+    close(i);
   execv(SPATH, myargv);
 
   exit(-1);
