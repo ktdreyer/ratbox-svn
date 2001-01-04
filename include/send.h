76e064c03096ac308f2072b5922318380d8a8272
a1kmm <a1kmm@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-01-04 19:24:46 +0000

    - Use GNU format checking features if __GNUC__ is defined - helps us find bugs in printf like send functions.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@4079 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/send.h
+++ include/send.h
@@ -25,6 +25,7 @@ unsigned long current_serial;
 
 extern void send_queued_write(int fd, void *data);
 
+#ifndef __GNUC__
 extern  void sendto_all_local_opers(struct Client *, char *message_type,
 				    const char *pattern,...);
 extern  void sendto_one(struct Client *, const char *, ...);
@@ -39,11 +40,40 @@ extern  void sendto_common_channels_local(struct Client *, const char *, ...);
 extern  void sendto_channel_local(int type,
 				  struct Channel *,
 				  const char *, ...);
+#else /* !__GNUC__*/
+extern  void sendto_all_local_opers(struct Client *, char *message_type,
+				    const char *pattern,...)
+	    __attribute__((format (printf, 3, 4)));
+extern  void sendto_one(struct Client *, const char *, ...)
+	    __attribute__((format (printf, 2, 3)));
+
+extern  void sendto_channel_butone(struct Client *, struct Client *, 
+                                   struct Channel *, const char *, ...)
+	    __attribute__((format (printf, 4, 5)));
+extern  void sendto_serv_butone(struct Client *, const char *, ...)
+	    __attribute__((format (printf, 2, 3)));
+extern  void sendto_ll_serv_butone(struct Client *, struct Client *sptr, int,
+				   const char *, ...)
+	    __attribute__((format (printf, 4, 5)));
+				   
+extern  void sendto_cap_serv_butone(int, struct Client *, const char *, ...)
+	    __attribute__((format (printf, 3, 4)));
+extern  void sendto_nocap_serv_butone(int, struct Client *, const char *, ...)
+	    __attribute__((format (printf, 3, 4)));
+extern  void sendto_common_channels_local(struct Client *, const char *, ...)
+	    __attribute__((format (printf, 2, 3)));
+extern  void sendto_channel_local(int type,
+				  struct Channel *,
+				  const char *, ...)
+	    __attribute__((format (printf, 3, 4)));
+#endif /* __GNUC__*/
+
 #define ALL_MEMBERS  0
 #define NON_CHANOPS  1
 #define ONLY_CHANOPS_VOICED 2
 #define ONLY_CHANOPS 3
 
+#ifndef __GNUC__
 extern  void sendto_channel_remote(struct Channel *, struct Client *cptr, 
 				   const char *, ...);
 extern  void sendto_ll_channel_remote(struct Channel *, struct Client *cptr, 
@@ -51,6 +81,7 @@ extern  void sendto_ll_channel_remote(struct Channel *, struct Client *cptr,
 				      const char *, ...);
 extern  void sendto_match_cap_servs(struct Channel *, struct Client *, 
                                     int, const char *, ...);
+                                    
 extern  void sendto_match_butone(struct Client *, struct Client *, 
                                  char *, int, const char *, ...);
 
@@ -65,6 +96,45 @@ extern  void ts_warn(const char *, ...);
 
 extern  void sendto_anywhere(struct Client *, struct Client *, 
 			     const char *, ...);
+#else /* ! __GNUC__ */
+extern  void sendto_channel_remote(struct Channel *, struct Client *cptr, 
+				   const char *, ...)
+	    __attribute__((format (printf, 3, 4)));
+				   
+extern  void sendto_ll_channel_remote(struct Channel *, struct Client *cptr, 
+				      struct Client *sptr,
+				      const char *, ...)
+	    __attribute__((format (printf, 4, 5)));
+				      
+extern  void sendto_match_cap_servs(struct Channel *, struct Client *, 
+                                    int, const char *, ...)
+	    __attribute__((format (printf, 4, 5)));
+
+extern  void sendto_match_butone(struct Client *, struct Client *, 
+                                 char *, int, const char *, ...)
+	    __attribute__((format (printf, 5, 6)));
+
+extern  void sendto_realops(const char *, ...)
+	    __attribute__((format (printf, 1, 2)));
+
+extern  void sendto_realops_flags(int, const char *, ...)
+	    __attribute__((format (printf, 2, 3)));
+
+extern  void sendto_ops_butone(struct Client *, struct Client *, 
+                               const char *, ...)
+	    __attribute__((format (printf, 3, 4)));
+
+extern  void sendto_wallops_butone(struct Client *, struct Client *, 
+                                   const char *, ...)
+	    __attribute__((format (printf, 3, 4)));
+
+extern  void ts_warn(const char *, ...)
+	    __attribute__((format (printf, 1, 2)));
+
+extern  void sendto_anywhere(struct Client *, struct Client *, 
+			     const char *, ...)
+	    __attribute__((format (printf, 3, 4)));
+#endif
 
 /* used when sending to #mask or $mask */
 
