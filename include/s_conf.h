5efc6df97386987d1699b7ee98f01a9a66f2cda1
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-01-05 16:16:51 +0000

    - you can't safely re-use bitmasks!
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@4227 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/s_conf.h
+++ include/s_conf.h
@@ -116,8 +116,6 @@ struct ConfItem
 #define IsIllegal(x)    ((x)->status & CONF_ILLEGAL)
 
 /* aConfItem->flags */
-/* We can recycle flags as long as the same flag is not used twice for
- * the same type of config entry. */
 
 /* Generic flags... */
 /* access flags... */
@@ -132,9 +130,9 @@ struct ConfItem
 #define CONF_FLAGS_IDLE_LINED           0x0100
 #define CONF_FLAGS_SPOOF_IP             0x0200
 /* server flags */
-#define CONF_FLAGS_ALLOW_AUTO_CONN      0x0001
-#define CONF_FLAGS_LAZY_LINK            0x0002
-#define CONF_FLAGS_REDIR                0x0004
+#define CONF_FLAGS_ALLOW_AUTO_CONN      0x0400
+#define CONF_FLAGS_LAZY_LINK            0x0800
+#define CONF_FLAGS_REDIR                0x1000
 
 /* Macros for aConfItem */
 
