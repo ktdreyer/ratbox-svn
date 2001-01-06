38970b21d65f0c2c23feaf94fc8b01265d172cd3
ejb <ejb@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-01-06 02:18:19 +0000

    - make remote users actually get their ID set..
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@4283 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/s_user.c
+++ src/s_user.c
@@ -851,7 +851,9 @@ int do_remote_user(char* nick, struct Client* cptr, struct Client* sptr,
   user->server = find_or_add(server);
   strncpy_irc(sptr->host, host, HOSTLEN); 
   strncpy_irc(sptr->info, realname, REALLEN);
-
+  if (id)
+	  strcpy(sptr->user->id, id);
+  
 #if 0
   /* XXX dont do this (talk to is-) */
   /* if it has no ID, set the ID to the nick just in case */
