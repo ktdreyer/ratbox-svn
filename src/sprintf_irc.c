4b2ecc11dabf695742716c871b5718bc1418775d
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-01-16 15:41:53 +0000

    - reverted sprintf_irc.c   The entire point of sprintf_irc is to be much faster than sprintf   not to be a complete replacement for it. Treat it as another function   entirely that has a similar name.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@4615 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/sprintf_irc.c
+++ src/sprintf_irc.c
@@ -266,20 +266,13 @@ vsprintf_irc(register char *str, register const char *format,
 {
   register char c;
   register int bytes = 0;
-  int field_width;
+
   while ((c = *format++))
     {
-      field_width = 0;
       if (c == '%')
 	{
 	  c = *format++; /* May never be '\0' ! */
-      if (c == '0')
-        c = *format++;
-      if (IsDigit(c))
-        {
-         field_width = c - '0';
-         c = *format++;
-        }
+
 	  if (c == 's')
 	    {
 	      register const char *p1 = va_arg(args, const char *);
@@ -300,9 +293,7 @@ vsprintf_irc(register char *str, register const char *format,
 
 	      continue;
 	    }
-#if 0
-/* We don't really want all this stuff, it is better for it to be like
- * sprintf... -A1kmm */
+
 	  /*
 	   * Prints time_t value in interval
 	   * [ 100000000 , 4294967295 ]
@@ -357,7 +348,6 @@ vsprintf_irc(register char *str, register const char *format,
 
 	      continue;
 	    }
-#endif
 
 	  if (c == 'd')
 	    {
@@ -400,22 +390,17 @@ vsprintf_irc(register char *str, register const char *format,
 	      continue;
 	    }
 
-      if (c == 'u' || (c == 'l' && *format == 'u'))
+	  if (c == 'u')
 	    {
 	      register unsigned int v1, v2;
 	      register const char *ap;
 	      register char *s = &scratch_buffer[sizeof(scratch_buffer) - 2];
-
-          if (c == 'l')
-            format++;
+	      
 	      v1 = va_arg(args, unsigned int);
 	      if (v1 == 0)
 		{
 		  *str++ = '0';
-		  bytes++;
-		  if (field_width)
-  		  for (--field_width; field_width; bytes++, field_width--)
-		      *str++ = '0';
+		  ++bytes;
 		  continue;
 		}
 
@@ -430,24 +415,13 @@ vsprintf_irc(register char *str, register const char *format,
 	      while ((v1 = v2) > 0);
 	      
 	      while ('0' == *++s);
-	      /* Anyone see a better way of finding the number of 0s to add?
-	       * -A1kmm. */
-          v1 = sizeof(scratch_buffer) + scratch_buffer - 1 - s;
-          if (v1 < field_width)
-            v1 = field_width - v1;
-          else
-            v1 = 0;
-          while (v1--)
-            {
-              *str++ = '0';
-              bytes++;
-            }
+
 	      *str = *s;
 	      ++bytes;
+
 	      while ((*++str = *++s))
-	         {
-     		  ++bytes;
-     		}
+		++bytes;
+
 	      continue;
 	    }
 
