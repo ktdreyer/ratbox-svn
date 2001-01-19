92b18aaefcee923f7e6d3aef6bd53cfc63011130
adrian <adrian@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-01-19 10:52:39 +0000

    Initial pass at the select code rewrite.
    * it isn't multilist like the poll code. that would be kind of stupid
      considering that the fd set size is a function of the maximum open fd,
      rather than the number of fds in a "set".
    
    * for some reason the compiler on my laptop is smoking heavy crack with
      hybrid-7 now. It seems to work, but debugging shows a lot of gremlins.
      I'll try it on my -stable box now that I'm back online and fix any
      last minute bugs.
    
    
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@4743 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/s_bsd_select.c
+++ src/s_bsd_select.c
@@ -3,12 +3,6 @@
  * s_bsd_select.c - code implementing a select IO loop
  *   By Adrian Chadd <adrian@creative.net.au>
  *
- * Based upon:
- *
- *   IRC - Internet Relay Chat, src/s_bsd.c
- *   Copyright (C) 1990 Jarkko Oikarinen and
- *                      University of Oulu, Computing Center
- *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation; either version 1, or (at your option)
@@ -25,33 +19,12 @@
  *
  *  $Id$
  */
-
-#include <assert.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <netdb.h>
-#include <unistd.h>
-#include <time.h>
-#include <sys/stat.h>
-#include <sys/socket.h>
-#include <sys/file.h>
-#include <sys/ioctl.h>
-#include <sys/resource.h>
-#ifdef HAVE_SYS_PARAM_H
-#include <sys/param.h>    /* NOFILE */
-#endif
-#include <arpa/inet.h>
-
-#include "tools.h"
+#include "fdlist.h"
 #include "s_bsd.h"
 #include "class.h"
 #include "client.h"
 #include "common.h"
 #include "config.h"
-#include "fdlist.h"
 #include "irc_string.h"
 #include "ircdauth.h"
 #include "ircd.h"
@@ -71,48 +44,117 @@
 #include "s_bsd.h"
 #include "memory.h"
 
-#ifndef IN_LOOPBACKNET
-#define IN_LOOPBACKNET        0x7f
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/stat.h>
+#include <sys/socket.h>
+#include <sys/file.h>
+#include <sys/ioctl.h>
+#include <sys/resource.h>
+#ifdef HAVE_SYS_PARAM_H
+#include <sys/param.h>    /* NOFILE */
 #endif
-
-#ifndef INADDR_NONE
-#define INADDR_NONE ((unsigned int) 0xffffffff)
-#endif
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <time.h>
 
 /*
- * Stuff for select()
+ * Note that this is only a single list - multiple lists is kinda pointless
+ * under select because the list size is a function of the highest FD :-)
+ *   -- adrian
  */
 
-static fd_set readSet;
-static fd_set writeSet;
+fd_set select_readfds;
+fd_set select_writefds;
 
-fd_set*  read_set  = &readSet;
-fd_set*  write_set = &writeSet;
+/*
+ * You know, I'd rather have these local to comm_select but for some
+ * reason my gcc decides that I can't modify them at all..
+ *   -- adrian
+ */
+fd_set tmpreadfds;
+fd_set tmpwritefds;
 
-static void add_fds_from_list(dlink_list *list, unsigned char mask);
-static void check_fds_from_list(dlink_list *list, unsigned char mask,
-				int nfds);
+static void select_update_selectfds(int fd, short event, PF *handler);
 
+/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */
+/* Private functions */
+
+/*
+ * set and clear entries in the select array ..
+ */ 
+static void
+select_update_selectfds(int fd, short event, PF *handler)
+{  
+    /* Update the read / write set */
+    if (event || COMM_SELECT_READ) {
+        if (handler)
+            FD_SET(fd, &select_readfds);
+        else
+            FD_CLR(fd, &select_readfds);
+    }
+    if (event || COMM_SELECT_WRITE) {
+        if (handler)
+            FD_SET(fd, &select_writefds);
+        else
+            FD_CLR(fd, &select_writefds);
+    }
+}
+
+
+/* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */
+/* Public functions */
+
+
+/*
+ * init_netio
+ *
+ * This is a needed exported function which will be called to initialise
+ * the network loop code.
+ */
 void init_netio(void)
 {
-  if( MAXCONNECTIONS > FD_SETSIZE )
-    {
-      fprintf(stderr, "FD_SETSIZE = %d MAXCONNECTIONS = %d\n",
-             FD_SETSIZE, MAXCONNECTIONS);
-      fprintf(stderr,  
-        "Make sure your kernel supports a larger FD_SETSIZE then " \
-        "recompile with -DFD_SETSIZE=%d\n", MAXCONNECTIONS);
-      exit(-1);
+    FD_ZERO(&select_readfds);
+    FD_ZERO(&select_writefds);
+}
+
+/*
+ * comm_setselect
+ *
+ * This is a needed exported function which will be called to register
+ * and deregister interest in a pending IO state for a given FD.
+ */
+void
+comm_setselect(int fd, fdlist_t list, unsigned int type, PF * handler,
+    void *client_data, time_t timeout)
+{  
+    fde_t *F = &fd_table[fd];
+    assert(fd >= 0);
+    assert(F->flags.open);
+
+#ifdef NOTYET
+    debug(5, 5) ("commSetSelect: FD %d type %d, %s\n", fd, type, handler ? "SET"
+ : "CLEAR");
+#endif
+    if (type & COMM_SELECT_READ) {
+        F->read_handler = handler;
+        F->read_data = client_data;
+        select_update_selectfds(fd, COMM_SELECT_READ, handler);
     }
-  printf("Value of FD_SETSIZE is %d\n", FD_SETSIZE);
-
-  printf("AIEE! This code needs to be converted to use the new-style net " \
-         "IO. Go grab adrian.\n");
-  exit(59);
-
-  read_set  = &readSet;
-  write_set = &writeSet;
-  init_resolver();
+    if (type & COMM_SELECT_WRITE) {
+        F->write_handler = handler;
+        F->write_data = client_data;
+        select_update_selectfds(fd, COMM_SELECT_WRITE, handler);
+    }
+    if (timeout)
+        F->timeout = CurrentTime + timeout;
 }
  
 /*
@@ -120,277 +162,78 @@ void init_netio(void)
  * processed. Also check for connections with data queued and whether we can
  * write it out.
  */
-int read_message(time_t delay, unsigned char mask)        /* mika */
-
-     /* Don't ever use ZERO here, unless you mean to poll
-        and then you have to have sleep/wait somewhere 
-        else in the code.--msa
-      */
-{
-  struct Client*      cptr;
-  int                 nfds;
-  struct timeval      wait;
-  time_t              delay2 = delay;
-  time_t              now;
-  u_long              usec = 0;
-  int                 res;
-  int                 length;
-  struct AuthRequest* auth = 0;
-  struct AuthRequest* auth_next = 0;
-  struct Listener*    listener = 0;
-  int                 i;
-
-  now = CurrentTime;
-
-  for (res = 0;;)
-    {
-      FD_ZERO(read_set);
-      FD_ZERO(write_set);
-
-#ifdef USE_IAUTH
-      if (iAuth.socket != NOSOCK)
-      {
-      	if (IsIAuthConnect(iAuth))
-      		FD_SET(iAuth.socket, write_set);
-      	else
-        	FD_SET(iAuth.socket, read_set);
-      }
-#endif
-
-      for (auth = AuthPollList; auth; auth = auth->next) {
-        assert(-1 < auth->fd);
-        if (IsAuthConnect(auth))
-          FD_SET(auth->fd, write_set);
-        else /* if(IsAuthPending(auth)) */
-          FD_SET(auth->fd, read_set);
-      }
-      for (listener = ListenerPollList; listener; listener = listener->next) {
-        assert(-1 < listener->fd);
-        FD_SET(listener->fd, read_set);
-      }
-
-      add_fds_from_list(&lclient_list, mask);
-      add_fds_from_list(&serv_list, mask);
-      add_fds_from_list(&unknown_list, mask);
-
-      if (ResolverFileDescriptor >= 0)
-        {
-          FD_SET(ResolverFileDescriptor, read_set);
-        }
-      wait.tv_sec = IRCD_MIN(delay2, delay);
-      wait.tv_usec = usec;
-
-      nfds = select(MAXCONNECTIONS, read_set, write_set, 0, &wait);
-
-      if ((CurrentTime = time(NULL)) == -1)
-        {
-          log(L_CRIT, "Clock Failure");
-          restart("Clock failure");
-        }   
-
-      if (nfds == -1 && errno == EINTR)
-        {
-          return -1;
-        }
-      else if( nfds >= 0)
-        break;
-
-      res++;
-      if (res > 5)
-        restart("too many select errors");
-      sleep(10);
-    }
-
-  /*
-   * Check the name resolver
-   */
-
-  if (-1 < ResolverFileDescriptor && 
-      FD_ISSET(ResolverFileDescriptor, read_set)) {
-    get_res();
-    --nfds;
-  }
-  /*
-   * Check the auth fd's
-   */
-  for (auth = AuthPollList; auth; auth = auth_next) {
-    auth_next = auth->next;
-    assert(-1 < auth->fd);
-    if (IsAuthConnect(auth) && FD_ISSET(auth->fd, write_set)) {
-      send_auth_query(auth);
-      if (0 == --nfds)
-        break;
-    }
-    else if (FD_ISSET(auth->fd, read_set)) {
-      read_auth_reply(auth);
-      if (0 == --nfds)
-        break;
-    }
-  }
-  for (listener = ListenerPollList; listener; listener = listener->next) {
-    assert(-1 < listener->fd);
-    if (FD_ISSET(listener->fd, read_set))
-      accept_connection(listener);
-  }
-
-#ifdef USE_IAUTH
-  /*
-   * Check IAuth
-   */
-  if (iAuth.socket != NOSOCK)
-  {
-  	if (IsIAuthConnect(iAuth) && FD_ISSET(iAuth.socket, write_set))
-  	{
-  		/*FD_CLR(iAuth.socket, write_set);*/
-
-  		/*
-  		 * Complete the connection to the IAuth server
-  		 */
-  		if (!CompleteIAuthConnection())
-  		{
-  			close(iAuth.socket);
-  			iAuth.socket = NOSOCK;
-  		}
-  	}
-  	else if (FD_ISSET(iAuth.socket, read_set))
-    {
-      if (!ParseIAuth())
-      {
-        /*
-         * IAuth server closed the connection
-         */
-        close(iAuth.socket);
-        iAuth.socket = NOSOCK;
-      }
-    }
-  }
-#endif
-
-  check_fds_from_list(&lclient_list, mask, nfds);
-  check_fds_from_list(&serv_list, mask, nfds);
-  check_fds_from_list(&unknown_list, mask, nfds);
-
-  return 0;
-}
 
 /*
- * add_fds_from_list
+ * comm_select
  *
- * inputs	- pointer to a dlink_list
- *		- mask
- * output	- NONE
- * side effects	- adds fd's from clients on given list to fd_set's
+ * Do IO events
  */
-static void
-add_fds_from_list(dlink_list *list, unsigned char mask)
+
+int
+comm_select(time_t delay)
 {
-  dlink_node *ptr;
-  struct Client *cptr;
-  int i;
+    int num;
+    int fd;
+    PF *hdl;
+    fde_t *F;
+    struct timeval to;
 
-  for(ptr = list->head; ptr; ptr=ptr->next)
-    {
-      cptr = ptr->data;
+    /* update current time */
+    if ((CurrentTime = time(0)) == -1) {
+        log(L_CRIT, "Clock Failure");
+        restart("Clock failed");
+    }   
 
-      i = cptr->fd;	/* ugh */
+    /* Copy over the read/write sets so we don't have to rebuild em */
+    bcopy(&select_readfds, &tmpreadfds, sizeof(fd_set));
+    bcopy(&select_writefds, &tmpwritefds, sizeof(fd_set));
 
-      /* XXX */
-#if 0
-      if (!(fd_table[i].mask & mask))
-	continue;
-#endif
-#if 0
-      if (DBufLength(&cptr->localClient->buf_recvq) && delay2 > 2)
-	delay2 = 1;
-#endif
-
-      if (DBufLength(&cptr->localClient->buf_recvq) < 4088)        
-	{
-	  FD_SET(i, read_set);
-	}
-
-      /* XXX */
-      if (DBufLength(&cptr->localClient->buf_sendq || IsConnecting(cptr))
-	{
-	  FD_SET(i, write_set);
-	}
-    }
-}  
-
-/*
- * check_fds_from_list
- *
- * inputs	- pointer to a dlink_list
- * output	- NONE
- * side effects	- checks fd's from each clients on given list
- *		  does I/O if necessary
- */
-static void
-check_fds_from_list(dlink_list *list, unsigned char mask, int nfds)
-{
-  dlink_node *ptr;
-  struct Client *cptr;
-  int i;
-  int length;
-
-  for (ptr = list->head; ptr; ptr = ptr->next)
-    {
-      cptr = ptr->data;
-      i = cptr->fd;
-
-      if (nfds == 0)
-	return;
-
-#if 0
-      if (!(fd_table[i].mask & mask))
-	continue;
-#endif
-
-      /*
-       * See if we can write...
-       */
-      if (FD_ISSET(i, write_set)) {
-	--nfds;
-	if (IsConnecting(cptr)) {
-	  if (!completed_connection(cptr)) {
-	    exit_client(cptr, cptr, &me, "Lost C/N Line");
-	    continue;
-	  }
-	  send_queued(cptr);
-          if (!IsDead(cptr))
+    for (;;) {
+        to.tv_sec = 1;
+        to.tv_usec = 0;
+        num = select(highest_fd + 1, &tmpreadfds, &tmpwritefds, NULL, &to);
+        if (num >= 0)
+            break;
+        if (ignoreErrno(errno))
             continue;
-	}
-	else {
-	  /*
-	   * ...room for writing, empty some queue then...
-	   */
-	  send_queued(cptr);
-	  if (!IsDead(cptr))
-	    continue;
-	}
-	exit_client(cptr, cptr, &me, 
-		    (cptr->flags & FLAGS_SENDQEX) ? 
-		    "SendQ Exceeded" : strerror(get_sockerr(cptr->fd)));
-	continue;
-      }
-      length = 1;     /* for fall through case */
-
-      if (FD_ISSET(i, read_set)) {
-	--nfds;
-	length = read_packet(cptr);
-      }
-      else if (PARSE_AS_CLIENT(cptr) && !NoNewLine(cptr))
-	length = parse_client_queued(cptr);
-
-      if (length > 0 || length == CLIENT_EXITED)
-	continue;
-      if (IsDead(cptr)) {
-	exit_client(cptr, cptr, &me,
-                    strerror(get_sockerr(cptr->fd)));
-	continue;
-      }
-      error_exit_client(cptr, length);
-      errno = 0;
+        /* error! */
+        return -1;
+        /* NOTREACHED */
     }
+
+    /* update current time again, eww.. */
+    if ((CurrentTime = time(0)) == -1) {
+        log(L_CRIT, "Clock Failure");
+        restart("Clock failed");
+    }   
+
+    if (num == 0)
+        return 0;
+
+    /* XXX we *could* optimise by falling out after doing num fds ... */
+    for (fd = 0; fd < highest_fd + 1; fd++) {
+        F = &fd_table[fd];
+
+        if (FD_ISSET(fd, &tmpreadfds)) {
+            hdl = F->read_handler;
+            select_update_selectfds(fd, COMM_SELECT_READ, NULL);
+            if (!hdl) {
+                /* XXX Eek! This is another bad place! */
+            } else {
+                hdl(fd, F->read_data);
+            }
+        }
+        if (FD_ISSET(fd, &tmpwritefds)) {
+            hdl = F->write_handler;
+            select_update_selectfds(fd, COMM_SELECT_WRITE, NULL);
+            if (!hdl) {
+                /* XXX Eek! This is another bad place! */
+            } else {
+                hdl(fd, F->write_data);
+            }
+        }
+    }
+    return 0;
 }
+
 
