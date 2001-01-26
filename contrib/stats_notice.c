06b83a3b27c6d52f64b0e513dc3c57eab8715047
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-01-26 23:16:09 +0000

    - corrected formatting - added CVS $Id:$ string in comments
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@5001 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- contrib/stats_notice.c
+++ contrib/stats_notice.c
@@ -1,4 +1,7 @@
 /* copyright (c) 2000 Edward Brocklesby, Hybrid Development Team */
+/*
+ * $Id$
+ */
 
 #include "modules.h"
 #include "hook.h"
@@ -27,31 +30,31 @@ char *_version = "1.0";
 int
 show_stats(struct hook_stats_data *data)
 {
-	if (data->statchar == 'l' || data->statchar == 'L') 
-	{
-		if(data->name != NULL)
-			sendto_realops_flags(FLAGS_SPY,
-								 "STATS %c requested by %s (%s@%s) [%s] on %s",
-								 data->statchar,
-								 data->sptr->name,
-								 data->sptr->username,
-								 data->sptr->host,
-								 data->sptr->user->server,
-								 data->name);
-		else
-			sendto_realops_flags(FLAGS_SPY,
-								 "STATS %c requested by %s (%s@%s) [%s]",
-								 data->statchar,
-								 data->sptr->name,
-								 data->sptr->username,
-								 data->sptr->host,
-								 data->sptr->user->server);
-	}
-	else
-	{
-		sendto_realops_flags(FLAGS_SPY, "STATS %c requested by %s (%s@%s) [%s]",
-							 data->statchar, data->sptr->name, data->sptr->username,
-							 data->sptr->host, data->sptr->user->server);
-	}
-	return 0;
+  if (data->statchar == 'l' || data->statchar == 'L') 
+    {
+      if(data->name != NULL)
+	sendto_realops_flags(FLAGS_SPY,
+			     "STATS %c requested by %s (%s@%s) [%s] on %s",
+			     data->statchar,
+			     data->sptr->name,
+			     data->sptr->username,
+			     data->sptr->host,
+			     data->sptr->user->server,
+			     data->name);
+      else
+	sendto_realops_flags(FLAGS_SPY,
+			     "STATS %c requested by %s (%s@%s) [%s]",
+			     data->statchar,
+			     data->sptr->name,
+			     data->sptr->username,
+			     data->sptr->host,
+			     data->sptr->user->server);
+    }
+  else
+    {
+      sendto_realops_flags(FLAGS_SPY, "STATS %c requested by %s (%s@%s) [%s]",
+			   data->statchar, data->sptr->name, data->sptr->username,
+			   data->sptr->host, data->sptr->user->server);
+    }
+  return 0;
 }
