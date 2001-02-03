4141c01331ee9809d422eb36baf9cbbf601e7e94
davidt <davidt@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-02-03 02:38:24 +0000

    -I flags should be in CPPFLAGS, not CFLAGS, updated configure to stop warning from make depend
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@5184 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/config.h
+++ include/config.h
@@ -244,8 +244,7 @@
  * Define this to enable IPv6 support.  IPv6 is currently still being
  * developed, and is not guaranteed to work properly..
  */
-/* We probably don't want this on by default -A1kmm. */
-/* #define IPV6 */
+#define IPV6
 
 /*   STOP STOP STOP STOP STOP STOP STOP STOP STOP STOP STOP STOP STOP STOP  */
 
