f563990e14486b4a7386b3625bb44eee4b773684
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-03-14 21:17:29 +0000

    Adds strlcat and strlcpy for systems that don't have them.  configure now checks for the existance of such functions on the system.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@6012 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/irc_string.h
+++ include/irc_string.h
@@ -85,6 +85,16 @@ int inetpton(int af, const char *src, void *dst);
  * strncpy_irc - optimized strncpy
  */
 char* strncpy_irc(char* s1, const char* s2, size_t n);
+
+
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char *dst, const char *src, size_t siz)
+#endif
+
+#ifndef HAVE_STRLCAT
+size_t strlcat(char *dst, const char *src, size_t siz)
+#endif
+
 /*
  * clean_string - cleanup control and high ascii characters
  * -Dianora
