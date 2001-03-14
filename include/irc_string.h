2d8b07140d46fb6bb106937a33fdd0482e3efae6
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-03-14 21:50:04 +0000

    I forgot a ; on the strlcpy prototype.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@6013 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/irc_string.h
+++ include/irc_string.h
@@ -88,11 +88,11 @@ char* strncpy_irc(char* s1, const char* s2, size_t n);
 
 
 #ifndef HAVE_STRLCPY
-size_t strlcpy(char *dst, const char *src, size_t siz)
+size_t strlcpy(char *dst, const char *src, size_t siz);
 #endif
 
 #ifndef HAVE_STRLCAT
-size_t strlcat(char *dst, const char *src, size_t siz)
+size_t strlcat(char *dst, const char *src, size_t siz);
 #endif
 
 /*
