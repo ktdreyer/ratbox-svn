aea1a64ac62fcee6bf21e6b1dd15bccfd863a888
toot <toot@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-04-01 23:14:45 +0000

    fixes a couple of compile warnings in ircd.c..
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@6193 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/ircd.c
+++ src/ircd.c
@@ -249,25 +249,9 @@ daemon(int a, int b)
   fclose(stdout);
   fclose(stderr); */
 #endif
+  return 0;
 }
 #endif
-
-void
-cmd_enable_debug(char *what)
-{
-#ifdef DEBUGMODE
-  if(enable_debug(what) == -1)
-    {
-      fprintf(stderr, "ircd: '%s' is not a valid debug option\n", optarg);
-      exit(EXIT_FAILURE);
-    }
-  break;
-#else
-  fprintf(stderr, "ircd: not compiled in debug mode.\n");
-  fprintf(stderr, "ircd: exiting on error.\n");
-  exit(EXIT_FAILURE);
-#endif
-}
 
 static int printVersion = 0;
 
