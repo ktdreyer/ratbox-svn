5271a2d11d8f8b0cf44a459e8e8a373ad329e77e
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-04-03 02:35:52 +0000

    - Edward, could you review this?   this fixes botch with getops.h
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@6202 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/ircd.c
+++ src/ircd.c
@@ -37,9 +37,11 @@
 #include "setup.h"
 #include "config.h"
 
+#if 0
 #if defined(HAVE_GETOPT_H)
 #include <getopt.h>
 #endif /* HAVE_GETOPT_H */
+#endif
 
 #ifdef USE_GETTEXT
 #include <libintl.h>
@@ -84,10 +86,13 @@
 #include "memory.h"
 #include "hook.h"
 #include "debug.h"
+/* XXX perhaps should call this lgetopts.h instead? */
+#include "getopts.h"
 
 /*
+ * XXX Is this a redundant comment now?
  * for getopt
- * ZZZ this is going to need confirmation on other OS's
+ * XXX this is going to need confirmation on other OS's
  *
  * #include <getopt.h>
  * Solaris has getopt.h, you should too... hopefully
