5621bc12a95811dbbe0c62c82a3d1a569c560fb1
fl_ <fl_@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-04-19 20:15:12 +0000

    - *groan*  ok, this will make convertconf add logging/modules/general   blocks to the ircd.conf, after its converted it..
      we should probably encourage a bit more wider usage of it too..
    
    
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@6357 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- tools/convertconf.c
+++ tools/convertconf.c
@@ -46,6 +46,9 @@ struct ConnectPair
 static struct ConnectPair* base_ptr=NULL;
 
 static void ConvertConf(FILE* file,FILE *out);
+static void AddLoggingBlock(FILE* out);
+static void AddGeneralBlock(FILE* out);
+static void AddModulesBlock(FILE* out);
 static void usage(void);
 static char *getfield(char *);
 static void ReplaceQuotes(char *out, char *in);
@@ -77,8 +80,21 @@ int main(int argc,char *argv[])
     }
   
   ConvertConf(in,out);
-  puts("Please be sure to manually check and edit your configuration "
-         "file before using it.");
+
+  puts("Adding the logging/general/modules blocks to your config..");
+  AddLoggingBlock(out);
+  AddGeneralBlock(out);
+  AddModulesBlock(out);
+
+  puts("The config file has been converted however you MUST rearrange the config:"
+       "   o class blocks must be before anything that uses then (auth/connect)"
+       "   o auth blocks must be listed in order of importance"
+       "        - spoofs etc first"
+       "        - *@* last"
+       "     if this is NOT done, chances are spoofs wont work."
+       "   o the general/logging/modules parts will need to be edited"
+       "   o the config file needs to be checked manually.. as this"
+       "     doesnt have human brains ;P");
   return 0;
 }
 
@@ -86,6 +102,175 @@ static void usage()
 {
   fprintf(stderr,"convertconf ircd.conf.old ircd.conf.new\n");
   exit(-1);
+}
+
+static void AddLoggingBlock(FILE *out)
+{
+  fprintf(out, "logging {\n");
+  fprintf(out, "\t/* These three paths are not *CURRENTLY* used.  They are still\n");
+  fprintf(out, "\t * compiled into the ircd with config.h\n");
+  fprintf(out, "\t */\n\tlogpath = \"/var/log/ircd/\";\n\toper_log = \"oper.log\";\n");
+  fprintf(out, "\tgline_log = \"gline.log\";\n\n\t/* This option is used, however.\n");
+  fprintf(out, "\t * The following settings are valid (This can also be changed\n");
+  fprintf(out, "\t * by /quote SET LOG in the ircd)\n");
+  fprintf(out, "\t * L_CRIT, L_ERROR, L_WARN, L_NOTICE, L_TRACE, L_INFO, L_DEBUG\n");
+  fprintf(out, "\t */\n\tlog_level = L_INFO;\n};\n\n");
+}
+
+static void AddGeneralBlock(FILE *out)
+{
+  fprintf(out, "general {\n");
+
+  fprintf(out, "\t/* Specify the default setting of FLOODCOUNT at startup.\n");
+  fprintf(out, "\t * This is how many lines per second we allow before we throttle\n");
+  fprintf(out, "\t * users MSGs/Notices ONLY affects the startup value, otherwise\n");
+  fprintf(out, "\t * use: /quote set floodcount.\n\t*/\n");
+  fprintf(out, "\tdefault_floodcount=8;\n\n");
+
+  fprintf(out, "\t/* Send a notice to all opers on the server when someone tries\n");
+  fprintf(out, "\t * to OPER and uses the wrong password.\n\t*/\n");
+  fprintf(out, "\tfailed_oper_notice=yes;\n\n");
+
+  fprintf(out, "\t/* If failed_oper_notice is set to \"yes\", also notify when someone\n");
+  fprintf(out, "\t * fails to OPER because of an identity mismatch (wrong host or nick).\n\t*/\n");
+  fprintf(out, "\tfailed_oper_notice = yes;\n\n");
+
+  fprintf(out, "\t/* Define how many 'dot' characters are permitted in an ident reply\n");
+  fprintf(out, "\t * before rejecting the user.\n\t*/\n");
+  fprintf(out, "\tdots_in_ident=2;\n\n");
+
+  fprintf(out, "\t/* Define how many non-wildcard (not: '.' '*' '?' '@') characters are\n");
+  fprintf(out, "\t * needed in klines for them to be placed.  Does not affect\n");
+  fprintf(out, "\t * klines hand-placed in kline.conf.  eg to disallow *@*.com.\n\t*/\n");
+  fprintf(out, "\tmin_nonwildcard = 4;\n\n");
+
+  fprintf(out, "\t/* Enable the nick flood control code. */\n\tanti_nick_flood = yes;\n");
+  fprintf(out, "\t/* These settings will allow 5 nick changes in 20 seconds. */\n");
+  fprintf(out, "\tmax_nick_time = 20 seconds;\n\tmax_nick_changes = 5;\n\n");
+
+  fprintf(out, "\t/* Do not allow the clients exit message to be\n");
+  fprintf(out, "\t * sent to a channel if the client has been on for less than\n\t * anti_spam_exit_message_time\n");
+  fprintf(out, "\t * The idea is, some spambots exit with their spam, thus advertising\n");
+  fprintf(out, "\t * this way. (idea due to ThaDragon, I just couldn't find =his= code).\n\t*/\n");
+  fprintf(out, "\tanti_spam_exit_message_time = 5 minutes;\n\n");
+
+  fprintf(out, "\t/* Define the time delta permissible for a remote server connection.\n");
+  fprintf(out, "\t * If our timestamp and the remote server's timestamp are over\n");
+  fprintf(out, "\t * ts_max_delta different, the connection will be dropped.  If it\n");
+  fprintf(out, "\t * is less than ts_max_delta but more than ts_warn_delta, send\n");
+  fprintf(out, "\t * a notice to opers on the server, but still allow the connection.\n");
+  fprintf(out, "\tts_warn_delta = 30 seconds;\n\tts_max_delta = 5 minutes;\n\n");
+
+  fprintf(out, "\t/* When a user QUITs, prepend their QUIT message with \"Client exit:\"\n");
+  fprintf(out, "\t * in order to help prevent against faking server error messages (eg.\n");
+  fprintf(out, "\t * ping timeout, connection reset by peer).\n\t*/\n");
+  fprintf(out, "\tclient_exit = yes;\n\n");
+
+  fprintf(out, "\t/* Show the reason why the user was K-lined or D-lined to the \"victim\"\n");
+  fprintf(out, "\t * It's a neat feature except for one thing... If you use a tcm\n");
+  fprintf(out, "\t * and it shows the nick of the oper doing the kline (as it does by\n");
+  fprintf(out, "\t * default) Your opers can be hit with retaliation... Or if your\n");
+  fprintf(out, "\t * opers use scripts that stick an ID into the comment field. etc.\n\t*/\n");
+  fprintf(out, "\tkline_with_reason = yes;\n\n");
+
+  fprintf(out, "\t/* Force the sign-off reason to be \"Connection closed\" when a user is\n");
+  fprintf(out, "\t * K-lined.  The user will still see the real reason.  This prevents\n");
+  fprintf(out, "\t * other users seeing the client disconnect from harassing the IRCops.\n\t*/\n");
+  fprintf(out, "\tkline_with_connection_closed = no;\n\n");
+
+  fprintf(out, "\t/* Set to yes if you wish your server to flag and not apply redundant\n");
+  fprintf(out, "\t* K-lines.\n\t*/\n\tnon_redundant_klines = yes;\n\n");
+
+  fprintf(out, "\t/* Enable this if you want opers to get noticed about \"things\" trying\n");
+  fprintf(out, "\t * to connect as servers that don't have N: lines.  Twits with\n");
+  fprintf(out, "\t * misconfigured servers can get really annoying with this enabled.\n\t*/\n");
+  fprintf(out, "\twarn_no_nline = yes;\n\n");
+
+  fprintf(out, "\t/* Set STATS o to be oper only */\n\to_lines_oper_only=yes;\n\n");
+
+  fprintf(out, "\t/* Links Delay determines the time between updates of the user LINKS\n");
+  fprintf(out, "\t * file.  The default updates it every 5 minutes.\n\t*/\n\tlinks_delay = 5 minutes;\n\n");
+  
+  fprintf(out, "\t/* minimum time between uses of MOTD, INFO, HELP, LINKS, TRACE */\n\tpace_wait = 10 seconds;\n\n"); 
+
+  fprintf(out, "\t/* The minimum time  between uses of remote WHOIS before\n");
+  fprintf(out, "\t * the counter is reset.\n\t*/\n\twhois_wait = 1 second;\n\n");
+
+  fprintf(out, "\t/* Define the amount of time between KNOCKs.  Once every 5 minutes\n");
+  fprintf(out, "\t * should be enough.\n\t*/\n\tknock_delay = 5 minutes;\n\n");
+
+  fprintf(out, "\t/* Amount of time between notifying users in +g mode that someone\n");
+  fprintf(out, "\t * is messaging them.\n\t*/\n\tcaller_id_wait = 1 minute;\n\n");
+
+  fprintf(out, "\t/* There are clients ignoring the FORCE_MOTD numeric.  There is no\n");
+  fprintf(out, "\t * no point forcing MOTD on connecting clients IMO.  Give them a short\n");
+  fprintf(out, "\t * NOTICE telling them they should read the MOTD, and leave it at that.\n");
+  fprintf(out, "\t*/\n\tshort_motd = no;\n\n");
+
+  fprintf(out, "\t/* Set to yes to disable flood control for opers. */\n\tno_oper_flood = yes;\n\n");
+  fprintf(out, "\t/* stop banned people from being able to talk in channels. */\n");
+  fprintf(out, "\tquiet_on_ban = no;\n\n");
+
+  fprintf(out, "\t/* Enable G-lines */\n\tglines = yes;\n\n");
+
+  fprintf(out, "\t/* Set the time for how long G-lines will last.\n\tgline_time = 1 day;\n\n");
+
+  fprintf(out, "\t/* Define the maximum amount of time a user can idle before\n");
+  fprintf(out, "\t* disconnecting them.  Set to 0 to disable.\n\t*/\n\tidletime = 0;\n\n");
+
+  fprintf(out, "\t/* Enable the server hiding feature.  This prevents users from\n");
+  fprintf(out, "\t * finding out what server users are on, and hides IP's.  Note\n");
+  fprintf(out, "\t * that some clients do not handle this well and may break.  Blame\n");
+  fprintf(out, "\t * the packet kiddies for making this option almost a necessity.\n\t*/\n");
+  fprintf(out, "\thide_server = yes;\n\n");
+
+  fprintf(out, "\t/* How many servers to autoconnect to if theres no class\n");
+  fprintf(out, "\tmaximum_links = 1;\n\n");
+
+  fprintf(out, "\t/* Define these to the log files you want to use for user connections\n");
+  fprintf(out, "\t * (userlog), successful use of /oper (operlog), and failed use of\n");
+  fprintf(out, "\t * /oper (foperlog).  Logging will stop if either these files do not\n");
+  fprintf(out, "\t * exist, or if they are not defined.\n\t*/\n\tfname_userlog = \"logs/userlog\";\n");
+  fprintf(out, "\tfname_operlog = \"logs/operlog\";\n\tfname_foperlog = \"logs/foperlog\";\n\n");
+
+  fprintf(out, "\t/* max_targets\n\t * only max_target targets can be PRIVMSG'ed / NOTICE'd in a single\n");
+  fprintf(out, "\t * command. default is 4 if not defined here. setting to 0 will have\n");
+  fprintf(out, "\t * broken results (ie PRIVMSG/NOTICE won't work).\n\t*/\n\tmax_targets = 4;\n\n");
+
+  fprintf(out, "\t/* message_locale\n\t * default message locale to use if gettext() is enabled\n");
+  fprintf(out, "\t * Use \"custom\" for the (in)famous Hybrid custom messages.\n");
+  fprintf(out, "\t * Use \"standard\" for the compiled in defaults.\n\t*/\n\tmessage_locale = \"custom\";\n\n");
+
+  fprintf(out, "\t/* List of user modes that only opers can set, see example.conf for a list. */\n");
+  fprintf(out, "\toper_umodes = locops, servnotice, operwall, wallop\n\n");
+
+  fprintf(out, "\t/* List of usermodes that get set when a user /oper's */\n");
+  fprintf(out, "\toper_umodes = locops, servnotice, operwall, wallop\n\n");
+
+  fprintf(out, "\t/* vchans_oper_only\n\t * dont allow non-opers to use CJOIN?\n\t*/\n");
+  fprintf(out, "\tvchans_oper_only = yes;\n\n");
+
+  fprintf(out, "\t/* disable_vchans\n");
+  fprintf(out, "\t * disable the creation of vchans by remote servers.. we do not\n");
+  fprintf(out, "\t * currently recommend this feature, as it removes a lot of\n");
+  fprintf(out, "\t * functionality, however it's here for those who don't want\n");
+  fprintf(out, "\t * vchan support at all.\n\t*/\n\tdisable_vchans = no;\n");
+
+  fprintf(out, "};\n\n");
+}
+
+static void AddModulesBlock(FILE* out)
+{
+  fprintf(out, "modules {\n");
+  fprintf(out, "\t/* set paths for module.  these paths are searched both for\n");
+  fprintf(out, "\t * module=\"\" and /quote modload, when a relative pathname\n");
+  fprintf(out, "\t * is specified.\n\t*/\n");
+
+  fprintf(out, "\tpath = \"/usr/local/ircd/modules/local\";\n");
+  fprintf(out, "\tpath = \"/usr/local/ircd/modules/autoload\";\n\n");
+
+  fprintf(out, "\t/* load a module upon startup (or rehash) */\n");
+  fprintf(out, "\tmodule = \"some_module.so\";\n};\n");
 }
 
 /*
@@ -292,9 +477,9 @@ static void oldParseOneLine(FILE *out,char* line)
       if(host_field)
 	fprintf(out,"\tname=\"%s\";\n", passwd_field);
       if(user_field)
-	fprintf(out,"\temail=\"%s\";\n", user_field);
+	fprintf(out,"\tdescription=\"%s\";\n", user_field);
       if(passwd_field)
-	fprintf(out,"\tdescription=\"%s\";\n", host_field);
+	fprintf(out,"\temail=\"%s\";\n", host_field);
       fprintf(out,"};\n\n");
       break;
 
@@ -316,11 +501,9 @@ static void oldParseOneLine(FILE *out,char* line)
       break;
 
     case 'd':
-      fprintf(out,"acl_exception {\n");
+      fprintf(out,"exempt {\n");
       if(user_field)
 	fprintf(out,"\tip=\"%s\";\n", user_field);
-      if(passwd_field)
-	fprintf(out,"\treason=\"%s\";\n", passwd_field);
       fprintf(out,"};\n\n");
       break;
 
@@ -413,6 +596,10 @@ static void oldParseOneLine(FILE *out,char* line)
 	fprintf(out,"\thub=yes;\n");
       else
      	fprintf(out,"\thub=no;\n");
+      /* Also print a default servername/netname */
+      fprintf(out, "\tnetwork_name=\"EFNet\";\n");
+      fprintf(out, "\tnetwork_desc=\"Eris Free Network\";\n");
+      fprintf(out, "\tmax_clients=1024;\n");
       fprintf(out,"};\n\n");
       break;
 
@@ -478,10 +665,8 @@ static void oldParseOneLine(FILE *out,char* line)
     case 'p':
       fprintf(out,"listen {\n");
       /* What is the purpose of this field? */
-      if(host_field)
+      if(host_field && *host_field)
 	fprintf(out,"\tname=\"%s\";\n", host_field);
-      if(passwd_field)
-        fprintf(out,"\tip=\"%s\";\n", passwd_field);
       if(port_field)
 	fprintf(out,"\tport=%d;\n", atoi(port_field));
       fprintf(out,"};\n\n");
@@ -489,10 +674,17 @@ static void oldParseOneLine(FILE *out,char* line)
 
     case 'Q': /* reserved nicks */
     case 'q': 
-      fprintf(out,"quarantine {\n");
-      if(host_field)
-	fprintf(out,"\tname=\"%s\";\n", host_field);
-      if(passwd_field)
+      if(host_field && (*host_field != '#'))
+        {
+          fprintf(out,"quarantine {\n");
+          fprintf(out,"\tname=\"%s\";\n", host_field);
+        }
+      else
+        {
+          puts("Cannot convert a channel quarantine, skipping");
+          break;
+        } 
+     if(passwd_field)
 	fprintf(out,"\treason=\"%s\";\n", passwd_field);
       fprintf(out,"};\n\n");
       break;
@@ -517,9 +709,14 @@ static void oldParseOneLine(FILE *out,char* line)
       if(passwd_field)
 	fprintf(out,"\treason=\"%s\";\n", passwd_field);
       if(port_field)
-        fprintf(out,"\taction=reject;\n");
-      else
-        fprintf(out,"\taction=warn;\n");
+        {
+          if (*user_field == '0')
+            fprintf(out,"\taction=reject;\n");
+          else if (*user_field == '1')
+            fprintf(out,"\taction=warn;\n");
+          else
+            fprintf(out, "\taction=silent;\n");
+        }
       fprintf(out,"};\n\n");
       break;
 
@@ -589,10 +786,10 @@ static void PrintOutServers(FILE* out)
 	  if(p->compressed)
 	    fprintf(out,"\tcompressed=yes;\n");
 #endif
-
+#if 0
 	  if(p->lazylink)
 	    fprintf(out,"\tlazylink=yes;\n");
-
+#endif
 	  if(p->hub_mask)
 	    {
 	      fprintf(out,"\thub_mask=\"%s\";\n",p->hub_mask);
@@ -745,6 +942,7 @@ static void OperPrivsFromString(FILE* out, char *privs)
 {
   while(*privs)
     {
+      /* If the priv is lower case, it defaults to whatever=no; anyway, so we dont need this? --fl */
       if(*privs == 'O')                     /* allow global kill */
 	{
 	  fprintf(out,"\tglobal_kill=yes;\n");
@@ -773,17 +971,13 @@ static void OperPrivsFromString(FILE* out, char *privs)
 	{
 	  fprintf(out,"\tnick_changes=yes;\n");
 	}
-      else if(*privs == 'n')                /* disallow +n see nick changes */
-	{
-	  fprintf(out,"\tnick_changes=no;\n");
-	}
       else if(*privs == 'K')                /* allow kill and kline privs */
 	{
 	  fprintf(out,"\tkline=yes;\n");
 	}
       else if(*privs == 'k')                /* disallow kill and kline privs */
 	{
-	  fprintf(out,"\tkline_kill=no;\n");
+	  fprintf(out,"\tkline=no;\n");
 	}
       else if(*privs == 'G')                /* allow gline */
 	{
@@ -826,8 +1020,11 @@ static char* ClientFlags(FILE *out, char* spoof, char *tmp)
         {
         case '=':
 	  if(spoof)
-	    fprintf(out,"\tspoof=\"%s\";\n",spoof);	  
-          break;
+            {
+	      fprintf(out,"\tspoof=\"%s\";\n",spoof);	  
+              fprintf(out,"\tspoof_notice=yes;\n");
+            }
+              break;
 	case '!':
 #if 0
           /* This is gone */
@@ -866,6 +1063,9 @@ static char* ClientFlags(FILE *out, char* spoof, char *tmp)
 	  fprintf(out,"\tcan_idle=yes;\n");	  
 #endif
           break;
+         case '_':     /* gline exempt */
+           fprintf(out, "\tgline_exempt=yes;\n");
+           break;
         default:
           return tmp;
         }
