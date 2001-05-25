8e61f77f232da847a17a2c6c16e23240022d47f6
davidt <davidt@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-05-25 15:30:52 +0000

    - Erp, we now set fds 3..LAST_SLINK_FD non blocking, not 0..LAST_SLINK_FD,   somehow FreeBSD-CURRENT doesn't care, but FreeBSD-STABLE really really   does...
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@6991 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/s_serv.c
+++ src/s_serv.c
@@ -1318,7 +1318,7 @@ int fork_server(struct Client *server)
     if(dup2(data_pipe[0], 4) < 0)
       exit(1);
     if(dup2(server->fd, 5) < 0)
-      return -1;
+      exit(1);
 #ifdef MISSING_SOCKPAIR
     /* only uni-directional pipes, so use 6/7 for writing */
     if(dup2(ctrl_pipe2[1], 6) < 0)
@@ -1327,7 +1327,7 @@ int fork_server(struct Client *server)
       exit(1);
 #endif
 
-    for(i = 0; i <= LAST_SLINK_FD; i++)
+    for(i = 3; i <= LAST_SLINK_FD; i++)
       if(!set_non_blocking(i))
         exit(1);
     for(i = (LAST_SLINK_FD + 1); i < MAXCONNECTIONS; i++)
