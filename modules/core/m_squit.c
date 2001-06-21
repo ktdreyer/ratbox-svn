c5b6c75f6754a32843e55df03ab95a1877005ceb
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-06-21 18:38:45 +0000

    - fixed serverhide bug in squit
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@7611 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/core/m_squit.c
+++ modules/core/m_squit.c
@@ -138,7 +138,7 @@ static void ms_squit(struct Client *client_p, struct Client *source_p,
 	  sendto_wallops_flags(FLAGS_WALLOP, &me,
 				 "Remote SQUIT %s from %s (%s)",
 				 found_squit->server_name,
-				 get_client_name(source_p, HIDE_IP), comment);
+				 source_p->name, comment);
 
           sendto_server(NULL, NULL, NULL, NOCAPS, NOCAPS, NOFLAGS,
                         ":%s WALLOPS :Remote SQUIT %s from %s (%s)",
