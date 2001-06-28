d39eba340c2f94c3f1e9d794c909ad302849a641
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-06-28 22:57:07 +0000

    - I should probably actually add the files ;p - made the makefiles compile them too
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@7709 b93f080a-4cfa-0310-a19a-80a68f531ef9

---
+++ include/resv.h
@@ -0,0 +1,52 @@
+/************************************************************************
+ *   IRC - Internet Relay Chat, include/resv.h
+ *   Copyright (C) 1991 Hybrid Development Team
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *   $Id$
+ */
+#ifndef INCLUDED_resv_h
+#define INCLUDED_resv_h
+#ifndef INCLUDED_sys_types_h
+#include <sys/types.h>
+#define INCLUDED_sys_types_h
+#endif
+
+struct Resv
+{
+  struct Resv *next;
+  struct Resv *prev;
+  struct Resv *hnext;
+
+  char	name[CHANNELLEN];
+  int	type;
+  int	conf;
+};
+
+#define RESV_NICK 0
+#define RESV_CHANNEL 1
+
+extern struct Resv *ResvList;
+
+extern struct Resv *create_resv(char *, int, int);
+extern int delete_resv(struct Resv *);
+extern int clear_conf_resv();
+extern int find_resv(char *name, int type);
+
+#endif  /* INCLUDED_hash_h */
+
+
+
---
+++ src/resv.c
@@ -0,0 +1,106 @@
+/*
+ * resv.c
+ *
+ * $Id$
+ */
+#include "tools.h"
+#include "restart.h"
+#include "common.h"
+#include "fdlist.h"
+#include "ircd.h"
+#include "send.h"
+#include "s_debug.h"
+#include "s_log.h"
+#include "client.h"   
+#include "memory.h"
+#include "resv.h"
+#include "ircd_defs.h"
+
+#include <unistd.h>
+#include <stdlib.h>
+
+struct Resv *ResvList;
+
+struct Resv *create_resv(char *name, int type, int conf)
+{
+  struct Resv *resv_p;
+  int len;
+
+  len = strlen(name);
+  
+  if ((type == RESV_CHANNEL) && (len > CHANNELLEN))
+  {
+    len = CHANNELLEN;
+    *(name + CHANNELLEN) = '\0';
+  }
+  else if ((type == RESV_NICK) && (len > NICKLEN))
+  {
+    len = NICKLEN;
+    *(name + NICKLEN) = '\0';
+  }
+
+  if(resv_p = hash_find_resv((const char *)name, (struct Resv *)NULL, type))
+    return NULL;
+
+  resv_p = (struct Resv *)MyMalloc(sizeof(struct Resv) + len + 1);
+  
+  strcpy(resv_p->name, name);
+  resv_p->type = type;
+  resv_p->conf = conf;
+
+  if(ResvList)
+    ResvList->prev = resv_p;
+
+  resv_p->next = ResvList;
+  resv_p->prev = NULL;
+
+  ResvList = resv_p;
+
+  add_to_resv_hash_table(resv_p->name, resv_p);
+
+  return resv_p;
+}
+
+int clear_conf_resv()
+{
+  struct Resv *resv_p;
+  struct Resv *next_p;
+
+  for(resv_p = ResvList; resv_p; resv_p = next_p)
+  {
+    next_p = resv_p->next;
+
+    if(resv_p->conf)
+      delete_resv(resv_p);
+  }
+}
+
+int delete_resv(struct Resv *resv_p)
+{
+  if(!(resv_p))
+    return 0;
+
+  del_from_resv_hash_table(resv_p->name, resv_p, resv_p->type);
+  
+  if(resv_p->prev)
+    resv_p->prev->next = resv_p->next;
+  else
+    ResvList = resv_p->next;
+
+  if(resv_p->next)
+    resv_p->next->prev = resv_p->prev;
+
+  MyFree((char *)resv_p);
+
+  return 1;
+}    
+
+int find_resv(char *name, int type)
+{
+  struct Resv *resv_p;
+
+  if(!(resv_p = hash_find_resv((const char *)name, (struct Resv *)NULL, type)))
+    return 0;
+
+  return 1;
+}  
