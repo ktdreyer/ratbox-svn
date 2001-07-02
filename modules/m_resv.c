b41bded422217ee0b0d002f62eac56865566d180
jdc <jdc@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-07-02 04:56:17 +0000

    - modules/m_resv.c     * Missing include for "hash.h" -- warnings...
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@7739 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_resv.c
+++ modules/m_resv.c
@@ -33,6 +33,7 @@
 #include "modules.h"
 #include "s_conf.h"
 #include "resv.h"
+#include "hash.h"
 
 static void mo_resv(struct Client *, struct Client *, int, char **);
 static void mo_unresv(struct Client *, struct Client *, int, char **);
