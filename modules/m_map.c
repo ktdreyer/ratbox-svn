8388af69506762926c4f1750d019a760c91c652e
davidt <davidt@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-09-16 21:40:06 +0000

    - Tidy up m_map.c slightly, and add missing RPL_MAPEND.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@8469 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_map.c
+++ modules/m_map.c
@@ -33,28 +33,30 @@
 #include "send.h"
 #include "s_conf.h"
 
+#define USER_COL       50 /* display | Users: %d at col 50 */
+
 static void m_map(struct Client *client_p, struct Client *source_p,
                     int parc, char *parv[]);
 static void mo_map(struct Client *client_p, struct Client *source_p,
                     int parc, char *parv[]);
 static void dump_map(struct Client *client_p,struct Client *root, char *pbuf);
+
 struct Message map_msgtab = {
   "MAP", 0, 1, 0, MFLG_SLOW, 0,
   {m_unregistered, m_map, m_ignore, mo_map}
 };
-void
-_modinit(void)
+
+void _modinit(void)
 {
   mod_add_cmd(&map_msgtab);
 }
 
-void
-_moddeinit(void)
+void _moddeinit(void)
 {
   mod_del_cmd(&map_msgtab);
 }
 
-char *_version = "20010818";
+char *_version = "20010913";
 static char buf[BUFSIZE];
 
 /* m_map
@@ -66,6 +68,7 @@ static void m_map(struct Client *client_p, struct Client *source_p,
   if (!ConfigServerHide.flatten_links)
   {
     dump_map(client_p,&me,buf);
+    sendto_one(client_p, form_str(RPL_MAPEND), me.name, client_p->name);
     return;
   }
 
@@ -81,7 +84,7 @@ static void mo_map(struct Client *client_p, struct Client *source_p,
                     int parc, char *parv[])
 {
   dump_map(client_p,&me,buf);
-  sendto_one(client_p, form_str(RPL_MAPEND), me.name,client_p->name);
+  sendto_one(client_p, form_str(RPL_MAPEND), me.name, client_p->name);
 }
 
 /*
@@ -90,8 +93,6 @@ static void mo_map(struct Client *client_p, struct Client *source_p,
 */
 static void dump_map(struct Client *client_p,struct Client *root_p, char *pbuf)
 {
-  int pad = 50;    /* position on which we add | Users: */
-  
   int cnt = 0, i = 0, len;
   int users = 0;
   struct Client *server_p,*user_p;
@@ -102,9 +103,9 @@ static void dump_map(struct Client *client_p,struct Client *root_p, char *pbuf)
   len = strlen(buf);
   buf[len] = ' ';
 	
-  if (len < pad)
+  if (len < USER_COL)
   {
-     for (i = len+1 ; i < pad ; i++)
+     for (i = len+1; i < USER_COL; i++)
      {
        buf[i] = '-';
      }
@@ -114,7 +115,8 @@ static void dump_map(struct Client *client_p,struct Client *root_p, char *pbuf)
   for( user_p = root_p->serv->users; user_p; user_p = user_p->lnext )
     users++;
         
-  snprintf(buf + pad, BUFSIZE - pad ," | Users: %5d (%4.1f%%)",users,
+  snprintf(buf + USER_COL, BUFSIZE - USER_COL,
+           " | Users: %5d (%4.1f%%)", users,
            100 * (float) users / (float) Count.total);
         
   sendto_one(client_p, form_str(RPL_MAP),me.name,client_p->name,buf);
