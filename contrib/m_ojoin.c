bfc7be5c99ad692d889c15330213e44b8474a983
wcampbel <wcampbel@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-10-13 03:29:57 +0000

    Added the old m_cinfo and m_ojoin modules to our devel tree. They may not be the most up to date versions...but they are here
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@8791 b93f080a-4cfa-0310-a19a-80a68f531ef9

---
+++ contrib/m_ojoin.c
@@ -0,0 +1,183 @@
+/************************************************************************
+ *   IRC - Internet Relay Chat, contrib/m_clearchan.c
+ *   Copyright (C) 1990 Jarkko Oikarinen and
+ *                      University of Oulu, Computing Center
+ *
+ *   See file AUTHORS in IRC package for additional names of
+ *   the programmers. 
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 1, or (at your option)
+ *   any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *   $Id$
+ */
+#include "tools.h"
+#include "handlers.h"
+#include "channel.h"
+#include "client.h"
+#include "ircd.h"
+#include "numeric.h"
+#include "s_log.h"
+#include "s_serv.h"
+#include "send.h"
+#include "whowas.h"
+#include "irc_string.h"
+#include "hash.h"
+#include "msg.h"
+#include "parse.h"
+#include "modules.h"
+#include "vchannel.h"
+#include "list.h"
+
+#include <string.h>
+
+static void mo_ojoin(struct Client *client_p, struct Client *source_p,
+                         int parc, char *parv[]);
+
+struct Message ojoin_msgtab = {
+  "OJOIN", 0, 2, 0, MFLG_SLOW, 0,
+  {m_unregistered, m_not_oper, m_ignore, mo_ojoin}
+};
+
+void
+_modinit(void)
+{
+  mod_add_cmd(&ojoin_msgtab);
+}
+
+void
+_moddeinit(void)
+{
+  mod_del_cmd(&ojoin_msgtab);
+}
+
+char *_version = "20010104";
+
+/*
+** mo_ojoin
+**      parv[0] = sender prefix
+**      parv[1] = channel
+*/
+static void mo_ojoin(struct Client *client_p, struct Client *source_p,
+                        int parc, char *parv[])
+{
+  struct Channel *chptr, *root_chptr;
+  int on_vchan = 0;
+  int move_me = 0;
+
+  /* admins only */
+  if (!IsSetOperAdmin(source_p))
+    {
+      sendto_one(source_p, ":%s NOTICE %s :You have no A flag", me.name, parv[0]);
+      return;
+    }
+
+  /* XXX - we might not have CBURSTed this channel if we are a lazylink
+   * yet. */
+  if (*parv[1] != '#')
+    {
+      parv[1]++;
+      move_me = 1;
+    }
+
+  chptr= hash_find_channel(parv[1], NullChn);
+  root_chptr = chptr;
+  if (chptr && parc > 2 && parv[2][0] == '!')
+    {
+      chptr = find_vchan(chptr, parv[2]);
+      if (root_chptr != chptr)
+        on_vchan++;
+    }
+
+  if( chptr == NULL )
+    {
+      sendto_one(source_p, form_str(ERR_NOSUCHCHANNEL),
+		 me.name, parv[0], parv[1]);
+      return;
+    }
+
+  if(IsMember(source_p, chptr))
+    {
+      sendto_one(source_p, ":%s NOTICE %s :Please part %s before using OJOIN",
+                 me.name, source_p->name, parv[1]);
+      return;
+    }
+
+  if (move_me == 1)
+    parv[1]--;
+
+  if (*parv[1] == '@') 
+    {
+       add_user_to_channel(chptr, source_p, CHFL_CHANOP);
+       sendto_ll_channel_remote(chptr, client_p, source_p,
+               ":%s SJOIN %lu %s + :@%s", me.name, chptr->channelts,
+               chptr->chname, source_p->name);
+       sendto_channel_local(ALL_MEMBERS, chptr, ":%s!%s@%s JOIN %s",
+                       source_p->name,
+                       source_p->username,
+                       source_p->host,
+                       root_chptr->chname);
+       sendto_channel_local(ALL_MEMBERS, chptr, ":%s MODE %s +o %s",
+                       me.name, chptr->chname, source_p->name);
+
+    }
+  else if (*parv[1] == '%')
+    {
+       add_user_to_channel(chptr, source_p, CHFL_HALFOP);
+       sendto_ll_channel_remote(chptr, client_p, source_p,
+               ":%s SJOIN %lu %s + :%%%s", me.name, chptr->channelts,
+               chptr->chname, source_p->name);
+       sendto_channel_local(ALL_MEMBERS, chptr, ":%s!%s@%s JOIN %s",
+                       source_p->name,
+                       source_p->username,
+                       source_p->host,
+                       root_chptr->chname);
+       sendto_channel_local(ALL_MEMBERS, chptr, ":%s MODE %s +h %s",
+                       me.name, chptr->chname, source_p->name);
+    }
+  else if (*parv[1] == '+')
+    {
+       add_user_to_channel(chptr, source_p, CHFL_VOICE);
+       sendto_ll_channel_remote(chptr, client_p, source_p,
+               ":%s SJOIN %lu %s + :+%s", me.name, chptr->channelts,
+               chptr->chname, source_p->name);
+       sendto_channel_local(ALL_MEMBERS, chptr, ":%s!%s@%s JOIN %s",
+                       source_p->name,
+                       source_p->username,
+                       source_p->host,
+                       root_chptr->chname);
+       sendto_channel_local(ALL_MEMBERS, chptr, ":%s MODE %s +v %s",
+                       me.name, chptr->chname, source_p->name);
+    }
+  else
+    {
+       add_user_to_channel(chptr, source_p, CHFL_PEON);
+       sendto_ll_channel_remote(chptr, client_p, source_p,
+               ":%s SJOIN %lu %s + :%s", me.name, chptr->channelts,
+               chptr->chname, source_p->name);
+       sendto_channel_local(ALL_MEMBERS, chptr, ":%s!%s@%s JOIN %s",
+                       source_p->name,
+                       source_p->username,
+                       source_p->host,
+                       root_chptr->chname);
+    }
+
+
+  /* XXX - check this isn't too big above... */
+  if (on_vchan)
+    add_vchan_to_client_cache(source_p,root_chptr,chptr);
+
+  channel_member_names(source_p, chptr, chptr->chname);
+}
+
