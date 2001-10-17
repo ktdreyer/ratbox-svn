ec07d1cb4fc48965bc66da02a0df65eec6c69e6b
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-10-17 02:40:30 +0000

    - zero out config structs in case they are full of garbage
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@8808 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/ircd.c
+++ src/ircd.c
@@ -355,6 +355,7 @@ io_loop(void)
 static void initialize_global_set_options(void)
 {
   memset( &GlobalSetOptions, 0, sizeof(GlobalSetOptions));
+  memset( &ConfigFileEntry, 0, sizeof(ConfigFileEntry));
 
   GlobalSetOptions.maxclients = MAX_CLIENTS;
   GlobalSetOptions.autoconn = 1;
@@ -366,6 +367,8 @@ static void initialize_global_set_options(void)
     GlobalSetOptions.floodcount = ConfigFileEntry.default_floodcount;
   else
     GlobalSetOptions.floodcount = 10;
+
+  memset( &ConfigChannel, 0, sizeof(ConfigChannel));
 
  /* End of global set options */
 
