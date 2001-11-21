43c442279da9f3dc6fefea764c859ec6c6bfda79
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-11-21 20:04:55 +0000

    - updated the headers
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@9127 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_help.c
+++ modules/m_help.c
@@ -1,27 +1,10 @@
-/************************************************************************
- *   IRC - Internet Relay Chat, modules/m_help.c
- *   Copyright (C) 1990 Jarkko Oikarinen and
- *                      University of Oulu, Computing Center
- *
- *   See file AUTHORS in IRC package for additional names of
- *   the programmers. 
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 1, or (at your option)
- *   any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+/*
+ * modules/m_help.c
+ * Copyright (C) 2001 Hybrid Development Team
  *
  *   $Id$
  */
+ 
 #include "handlers.h"
 #include "client.h"
 #include "ircd.h"
