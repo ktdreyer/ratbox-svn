0abafd5fa546447aa430727919bb16fc6034672e
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-12-03 16:31:42 +0000

    Fixed a minor compile issue.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@9260 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/s_serv.h
+++ include/s_serv.h
@@ -72,12 +72,15 @@ struct Capability
 #define CAP_UID         0x00004000   /* Can do UIDs */
 #define CAP_ZIP         0x00008000   /* Can do ZIPlinks */
 #define CAP_ENC         0x00010000   /* Can do ENCrypted links */
+
 #define CAP_KNOCK	0x00020000   /* supports KNOCK */
+
 #define CAP_TBURST	0x00040000   /* supports TBURST */
+
 #define CAP_MASK        (CAP_QS  | CAP_EX   | CAP_CHW  | \
                          CAP_IE  | CAP_EOB  | CAP_KLN  | \
                          CAP_GLN | CAP_HOPS | CAP_AOPS | \
-                         CAP_UID | CAP_ZIP  | CAP_ENC  | \ 
+                         CAP_UID | CAP_ZIP  | CAP_ENC | \
                          CAP_KNOCK | CAP_TBURST)
 
 #ifdef HAVE_LIBZ
