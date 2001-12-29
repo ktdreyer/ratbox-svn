ccf8185eb4e1c3acbcc3fcc6a50fbf22801b2beb
davidt <davidt@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2001-12-29 17:24:45 +0000

    - Produce srandom seed slightly differently   (XOR tv_sec with tv_usec and pid)
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@9687 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/ircd.c
+++ src/ircd.c
@@ -558,7 +558,7 @@ int main(int argc, char *argv[])
 
   ServerRunning = 0;
   /* It ain't random, but it ought to be a little harder to guess */
-  srandom((SystemTime.tv_sec / SystemTime.tv_usec+1) * getpid() );
+  srandom(SystemTime.tv_sec ^ (SystemTime.tv_usec | (getpid() << 20)));
   memset(&me, 0, sizeof(me));
   memset(&meLocalUser, 0, sizeof(meLocalUser));
   me.localClient = &meLocalUser;
