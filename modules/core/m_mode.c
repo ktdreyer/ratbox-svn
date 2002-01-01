75266e7d3c3958736bea8d92ce19c13c5a0ff00c
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-01-01 18:50:30 +0000

    - cosmetic cleanup of m_mode.c
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@9750 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/core/m_mode.c
+++ modules/core/m_mode.c
@@ -80,7 +80,7 @@ static void m_mode(struct Client *client_p, struct Client *source_p,
   int n = 2;
   
   /* Now, try to find the channel in question */
-  if( !IsChanPrefix(parv[1][0]) )
+  if (!IsChanPrefix(parv[1][0]))
     {
       /* if here, it has to be a non-channel name */
       user_mode(client_p, source_p, parc, parv);
@@ -97,7 +97,7 @@ static void m_mode(struct Client *client_p, struct Client *source_p,
 	  
   chptr = hash_find_channel(parv[1]);
 
-  if(chptr == NULL)
+  if (chptr == NULL)
     {
       /* if chptr isn't found locally, it =could= exist
        * on the uplink. So ask.
@@ -110,7 +110,7 @@ static void m_mode(struct Client *client_p, struct Client *source_p,
       /* only send a mode upstream if a local client sent this request
        * -davidt
        */
-      if ( MyClient(source_p) && !ServerInfo.hub && uplink &&
+      if (MyClient(source_p) && !ServerInfo.hub && uplink &&
 	   IsCapable(uplink, CAP_LL))
 	{
 #if 0
@@ -149,7 +149,7 @@ static void m_mode(struct Client *client_p, struct Client *source_p,
                    parv[0], root->chname);
         return;
        }
-     if (!(chptr = map_vchan(root, target_p)))
+     if ((chptr = map_vchan(root, target_p)) == NULL)
        {
         sendto_one(source_p, form_str(ERR_NOSUCHCHANNEL), me.name,
                    parv[0], root->chname);
@@ -182,7 +182,7 @@ static void m_mode(struct Client *client_p, struct Client *source_p,
         }
     }
 
-  if(parc < n+1)
+  if (parc < n+1)
     {
       channel_modes(chptr, source_p, modebuf, parabuf);
       sendto_one(source_p, form_str(RPL_CHANNELMODEIS),
