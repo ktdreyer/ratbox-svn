9ba1dff7932255cda41a539ece87043d8cad3fa3
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-01-22 06:11:34 +0000

    Fixed the ping cookie bug..now we need to fix the negative user count crap..
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@9989 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/s_user.c
+++ src/s_user.c
@@ -295,8 +295,11 @@ int register_local_user(struct Client *client_p, struct Client *source_p,
   assert(NULL != source_p->localClient);
   assert(source_p->username != username);
 
+  if(ConfigFileEntry.ping_cookie && source_p->flags & FLAGS_PINGSENT && !(source_p->flags2 & FLAGS2_PING_COOKIE) )
+  {
+ 	return 0;
+  }
 
-  /* If the user  */
   if(ConfigFileEntry.ping_cookie && !(source_p->flags2 & FLAGS2_PING_COOKIE) && !source_p->random_ping)
   {
     source_p->random_ping = (unsigned long)random();
@@ -304,6 +307,7 @@ int register_local_user(struct Client *client_p, struct Client *source_p,
     source_p->flags |= FLAGS_PINGSENT;
     return 0;
   } 
+
 
   user->last = CurrentTime;
   /* Straight up the maximum rate of flooding... */
