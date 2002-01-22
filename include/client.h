a6c7a5e307a06651d933570ce43da76e072c3397
davidt <davidt@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-01-22 17:21:13 +0000

    - Add client_p->free_file and client_p->free_line.   When exit_client() is called, it records the line/file it was called   from in the client struct, and if the client has already been added   to the dead_list, it will fail an assert leaving free_[file|line] to   be examined. - This should be backed out before releasing, all changes are wrapped   in #ifdef DUPFREEDEBUG
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@9992 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/client.h
+++ include/client.h
@@ -60,6 +60,9 @@
 
 #define CLIENT_BUFSIZE 512      /* must be at least 512 bytes */
 
+/* DUPFREEDEBUG */
+#define DUPFREEDEBUG 1
+
 /*
  * pre declare structs
  */
@@ -216,6 +219,11 @@ struct Client
   unsigned long random_ping;
   
   struct LocalUser *localClient;
+
+#ifdef DUPFREEDEBUG
+  char          free_file[512];
+  unsigned long free_line;
+#endif
 };
 
 struct LocalUser
@@ -593,8 +601,16 @@ extern void           add_client_to_llist(struct Client** list,
                                           struct Client* client);
 extern void           del_client_from_llist(struct Client** list, 
                                             struct Client* client);
+#ifdef DUPFREEDEBUG
+#define exit_client(cptr,sptr,from,comment) \
+          _exit_client(cptr,sptr,from,comment,__FILE__,__LINE__)
+extern int            _exit_client(struct Client*, struct Client*,
+                                   struct Client*, const char *,
+                                   const char *, unsigned long);
+#else
 extern int            exit_client(struct Client*, struct Client*, 
-                                  struct Client*, const char* comment);
+                                  struct Client*, const char*);
+#endif
 
 
 extern void     count_local_client_memory(int *count, int *memory);
