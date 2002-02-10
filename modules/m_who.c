50f84fd241c38f8675fd9e55ac43f656cb6a2b64
db <db@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-02-10 21:24:34 +0000

    - Stop a /who 0 coring   it would not core on fbsd because of libc, but does on solaris.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@10131 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_who.c
+++ modules/m_who.c
@@ -113,7 +113,7 @@ static void m_who(struct Client *client_p,
 
       if (*mask == '\0')
 	{
-	  sendto_one(source_p, form_str(RPL_ENDOFWHO), me.name, parv[0], "*" );
+	  sendto_one(source_p, form_str(RPL_ENDOFWHO), me.name, parv[0], "*");
 	  return;
 	}
     }
@@ -187,7 +187,7 @@ static void m_who(struct Client *client_p,
 		do_who_on_channel(source_p, chptr, chptr->chname, NO, NO);
 	    }
 	}
-      sendto_one(source_p, form_str(RPL_ENDOFWHO), me.name, parv[0], mask );
+      sendto_one(source_p, form_str(RPL_ENDOFWHO), me.name, parv[0], mask);
       return;
     }
 
@@ -245,7 +245,7 @@ static void m_who(struct Client *client_p,
 	  do_who(source_p, target_p, NULL, "");
 	}
 
-      sendto_one(source_p, form_str(RPL_ENDOFWHO), me.name, parv[0], mask );
+      sendto_one(source_p, form_str(RPL_ENDOFWHO), me.name, parv[0], mask);
       return;
     }
   /* '/who 0' */
@@ -255,7 +255,9 @@ static void m_who(struct Client *client_p,
   }
   /* Wasn't a nick, wasn't a channel, wasn't a '*' so ... */
   who_global(source_p, mask, server_oper);
-  sendto_one(source_p, form_str(RPL_ENDOFWHO), me.name, parv[0], mask );
+  if(mask == NULL)
+    mask = "*";
+  sendto_one(source_p, form_str(RPL_ENDOFWHO), me.name, parv[0], mask);
 }
 
 /* who_common_channel
@@ -287,7 +289,7 @@ static void who_common_channel(struct Client *source_p,dlink_list chain,
 
      SetMark(target_p);
 
-     if (!mask ||
+     if ((mask == NULL) ||
           match(mask, target_p->name) || match(mask, target_p->username) ||
           match(mask, target_p->host) || match(mask, target_p->user->server) ||
 	  match(mask, target_p->info))
