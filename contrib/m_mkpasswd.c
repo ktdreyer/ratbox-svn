ef31331063246b9231e0fc013789ae1ff64688e0
wcampbel <wcampbel@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-02-25 23:05:00 +0000

    I gave NO ONE permission to put m_flags.c and m_mkpasswd.c under the GPL.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@10312 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- contrib/m_mkpasswd.c
+++ contrib/m_mkpasswd.c
@@ -1,23 +1,10 @@
 /*
- *  ircd-hybrid: an advanced Internet Relay Chat Daemon(ircd).
- *  m_mkpasswd.c: Encrypts a password.
+ *  m_mkpasswd.c: Encrypts a password online, DES or MD5.
  *
- *  Copyright (C) 2002 by the past and present ircd coders, and others.
+ *  Copyright 2002 W. Campbell and the ircd-hybrid development team
+ *  Based on mkpasswd.c, originally by Nelson Minar (minar@reed.edu)
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
- *  USA
+ *  You can use this code in any way as long as these names remain.
  *
  *  $Id$
  */
