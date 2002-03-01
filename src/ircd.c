b1670fba639599795d4b51cc266f849120dbf9f5
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-03-01 14:30:24 +0000

    Add a check for nanosleep(). If nanosleep() isn't found we fall back to using select() for the delay.  The main reason for this is the fact that usleep() tends to be implemented via SIGALRM.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@10369 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/ircd.c
+++ src/ircd.c
@@ -159,6 +159,21 @@ int splitchecking;
 int split_users;
 int split_servers;
 
+static int irc_sleep(unsigned long useconds)
+{
+#ifdef HAVE_NANOSLEEP
+	struct timespec t;
+	t.tv_sec = useconds / (unsigned long)  1000000;
+	t.tv_nsec = (useconds % (unsigned long) 1000000) * 1000 ;
+	return nanosleep(&t, (struct timespec *) NULL);
+#else
+	struct timeval t;
+	t.tv_sec = 0;
+	tv.tv_usec = useconds;
+	select(0, NULL, NULL, NULL, &t);
+#endif
+}
+
 /*
  * get_vm_top - get the operating systems notion of the resident set size
  */
@@ -346,7 +361,7 @@ io_loop(void)
       if(st > 250000)
       	st = 250000;
       
-      usleep(st);
+      irc_sleep(st);
 
       comm_select(0);
   
