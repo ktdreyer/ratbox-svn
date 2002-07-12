3b6472baebce4e6905dd69a1548a0b48e48163ec
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-07-12 01:01:22 +0000

    Instead of saying Client Exit: say Quit:, to be a little more consistant with other networks
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@11551 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/core/m_quit.c
+++ modules/core/m_quit.c
@@ -77,9 +77,9 @@ static void m_quit(struct Client *client_p,
   if (ConfigFileEntry.client_exit && comment[0])
     {
 #ifndef VMS
-      snprintf(reason, TOPICLEN, "Client Exit: %s", comment);
+      snprintf(reason, TOPICLEN, "Quit: %s", comment);
 #else
-      sprintf(reason, "Client Exit: %s", comment);
+      sprintf(reason, "Quit: %s", comment);
 #endif
       comment = reason;
     }
