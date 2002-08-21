c9dea3f91c7032c79037e6977df418c536abf271
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-08-21 02:58:24 +0000

    Lets try that again
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@12359 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/tools.h
+++ include/tools.h
@@ -101,7 +101,8 @@ void mem_frob(void *data, int len);
 /* Returns the list length */
 #define dlink_list_length(list) (list)->length
 
-#define dlinkDestroyNode(list, data) do { dlink_node *ptr = dlinkFindDelete(list, data); if(ptr != NULL) free_dlink_node(ptr); } while(0);
+#define dlinkFindDestroy(list, data) do { dlink_node *ptr = dlinkFindDelete(list, data); if(ptr != NULL) free_dlink_node(ptr); } while(0);
+
 /*
  * The functions below are included for the sake of inlining
  * hopefully this will speed up things just a bit
