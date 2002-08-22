987069485a887dccdd5a64a9bc869161eccd0684
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-08-22 23:18:54 +0000

    - fixed up ltrace to be more efficient
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@12447 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- contrib/m_ltrace.c
+++ contrib/m_ltrace.c
@@ -36,6 +36,7 @@
 #include "fdlist.h"
 #include "s_bsd.h"
 #include "s_serv.h"
+#include "s_conf.h"
 #include "send.h"
 #include "msg.h"
 #include "parse.h"
@@ -43,12 +44,11 @@
 
 
 static void m_ltrace(struct Client *, struct Client *, int, char **);
-static void mo_ltrace(struct Client*, struct Client*, int, char**);
 static void ltrace_spy(struct Client *);
 
 struct Message ltrace_msgtab = {
   "LTRACE", 0, 0, 0, 0, MFLG_SLOW, 0,
-  {m_unregistered, m_ltrace, mo_ltrace, mo_ltrace}
+  {m_unregistered, m_ltrace, m_ltrace, m_ltrace}
 };
 
 #ifndef STATIC_MODULES
@@ -73,30 +73,12 @@ static int report_this_status(struct Client *source_p, struct Client *target_p,i
                               int link_u_p, int link_u_s);
 
 /*
- * m_ltrace()
- *
- *	parv[0] = sender prefix
- *	parv[1] = target client/server to trace
- */
-static void m_ltrace(struct Client *client_p, struct Client *source_p,
-                    int parc, char *parv[])
-{
-  char *tname;
-
-  if (parc > 1)
-    tname = parv[1];
-  else
-    tname = me.name;
-  sendto_one(source_p, form_str(RPL_ENDOFTRACE), me.name, parv[0], tname);
-}
-
-/*
- * mo_ltrace
+ * m_ltrace
  *      parv[0] = sender prefix
  *      parv[1] = servername
  */
-static void mo_ltrace(struct Client *client_p, struct Client *source_p,
-                    int parc, char *parv[])
+static void m_ltrace(struct Client *client_p, struct Client *source_p,
+                     int parc, char *parv[])
 {
   struct Client       *target_p = NULL;
   char  *tname;
@@ -113,15 +95,19 @@ static void mo_ltrace(struct Client *client_p, struct Client *source_p,
   else
     tname = me.name;
 
-  if(!IsOper(source_p))
+  if(!IsOper(source_p) && ConfigServerHide.hide_servers)
   {
-    sendto_one(source_p, form_str(RPL_ENDOFTRACE), me.name, parv[0], tname);
-    return;
-  }
+    if(MyClient(source_p) && (irccmp(tname, source_p->name) == 0))
+      report_this_status(source_p, source_p, 0, 0, 0);
 
+    sendto_one(source_p, form_str(RPL_ENDOFTRACE),
+              me.name, parv[0], tname);
+  }
   if (parc > 2)
+  {
     if (hunt_server(client_p, source_p, ":%s LTRACE %s :%s", 2, parc, parv))
       return;
+  }
 
   switch (hunt_server(client_p, source_p, ":%s LTRACE :%s", 1, parc, parv))
     {
@@ -164,96 +150,77 @@ static void mo_ltrace(struct Client *client_p, struct Client *source_p,
   set_time();
 
   /* lusers cant issue ltrace.. */
-  if(!IsOper(source_p) || !dow)
-    {
-      const char* name;
-      const char* class_name;
-      char ipaddr[HOSTIPLEN];
-
+  if(!dow)
+  {
       target_p = find_client(tname);
       
       if(target_p && IsPerson(target_p)) 
-      {
-        name = get_client_name(target_p, HIDE_IP);
-        inetntop(target_p->localClient->aftype, &IN_ADDR(target_p->localClient->ip), ipaddr, HOSTIPLEN);
+        report_this_status(source_p, target_p, 0, 0, 0);
 
-        class_name = get_client_class(target_p);
-
-        if (IsOper(target_p))
-          sendto_one(source_p, form_str(RPL_TRACEOPERATOR),
-                     me.name, parv[0], class_name, name, 
-#ifndef HIDE_SPOOF_IPS
-                     MyOper(source_p) ? ipaddr :
-#endif
-		     (IsIPSpoof(target_p) ? "255.255.255.255" : ipaddr),
-                     CurrentTime - target_p->lasttime,
-                     (target_p->user) ? (CurrentTime - target_p->user->last) : 0);
-      }
-      
       sendto_one(source_p, form_str(RPL_ENDOFTRACE),me.name,
                  parv[0], tname);
       return;
-    }
+  }
 
   memset((void *)link_s,0,sizeof(link_s));
   memset((void *)link_u,0,sizeof(link_u));
 
-  /*
-   * Count up all the servers and clients in a downlink.
+  /* count up the servers behind the server links only if were going
+   * to be using them --fl
    */
   if (doall)
-   {
-    DLINK_FOREACH(ptr, global_client_list.head)
-     {
-      target_p = (struct Client *)ptr->data;
-      if (IsPerson(target_p))
-        {
-          link_u[target_p->from->localClient->fd]++;
-        }
-      else if (IsServer(target_p))
-	{
-	  link_s[target_p->from->localClient->fd]++;
-	}
-     }
-   }
-   
-  /* report all opers */
-  DLINK_FOREACH(ptr, lclient_list.head)
+  {
+    DLINK_FOREACH(ptr, global_serv_list.head)
     {
       target_p = ptr->data;
 
-      if(!IsOper(target_p))
-	continue;
-
-      if (!doall && wilds && !match(tname, target_p->name))
-        continue;
-
-      if (!dow && irccmp(tname, target_p->name))
-        continue;
-
-      report_this_status(source_p,target_p,dow,0,0);
+      link_u[target_p->from->localClient->fd] += target_p->serv->usercnt;
+      link_s[target_p->from->localClient->fd]++;
     }
+  }
+  
+  /* report all opers */
+  DLINK_FOREACH(ptr, oper_list.head)
+  {
+    target_p = ptr->data;
+
+    if(!doall && wilds && (match(tname, target_p->name) == 0))
+      continue;
+
+    report_this_status(source_p, target_p, 0, 0, 0);
+  }
 
   /* report all servers */
-  DLINK_FOREACH(ptr, serv_list.head)
+  if(!ConfigServerHide.hide_servers)
+  {
+    DLINK_FOREACH(ptr, serv_list.head)
     {
       target_p = ptr->data;
 
-      if (!doall && wilds && !match(tname, target_p->name))
-        continue;
-      if (!dow && irccmp(tname, target_p->name))
+      if (!doall && wilds && match(tname, target_p->name) == 0)
         continue;
 
-      report_this_status(source_p, target_p, dow,
-                               link_u[target_p->localClient->fd],
-                               link_s[target_p->localClient->fd]);
+      report_this_status(source_p, target_p, 0,
+                         link_u[target_p->localClient->fd],
+                         link_s[target_p->localClient->fd]);
     }
+  }
+
+  if(IsOper(source_p))
+  {
+    DLINK_FOREACH(ptr, unknown_list.head)
+    {
+      target_p = ptr->data;
+
+      if(!doall && wilds && match(tname, target_p->name) == 0)
+        continue;
+
+      report_this_status(source_p, target_p, 0, 0, 0);
+    }
+  }
 
   sendto_one(source_p, form_str(RPL_ENDOFTRACE),me.name, parv[0],tname);
 }
-
-
-
 
 /*
  * report_this_status
@@ -281,14 +248,20 @@ static int report_this_status(struct Client *source_p, struct Client *target_p,
     case STAT_CONNECTING:
       sendto_one(source_p, form_str(RPL_TRACECONNECTING), me.name,
                  source_p->name, class_name, 
-		 IsOperAdmin(source_p) ? name : target_p->name);
+#ifndef HIDE_SERVERS_IPS
+                 IsOperAdmin(source_p) ? name : 
+#endif
+                 target_p->name);
 		   
       break;
 
     case STAT_HANDSHAKE:
       sendto_one(source_p, form_str(RPL_TRACEHANDSHAKE), me.name,
                  source_p->name, class_name, 
-                 IsOperAdmin(source_p) ? name : target_p->name);
+#ifndef HIDE_SERVERS_IPS
+                 IsOperAdmin(source_p) ? name :
+#endif
+                 target_p->name);
 		   
       break;
       
@@ -297,17 +270,18 @@ static int report_this_status(struct Client *source_p, struct Client *target_p,
       break;
 
     case STAT_CLIENT:
+#ifndef HIDE_SPOOF_IPS
       if (IsAdmin(target_p))
 	sendto_one(source_p, form_str(RPL_TRACEOPERATOR),
                    me.name, source_p->name, class_name, name,
-#ifndef HIDE_SPOOF_IPS
-                   IsOperAdmin(source_p) ? ip : 
-#endif
+                   IsOperAdmin(source_p) ? ip :
                    (IsIPSpoof(target_p) ? "255.255.255.255" : ip),
                    CurrentTime - target_p->lasttime,
                    (target_p->user) ? (CurrentTime - target_p->user->last) : 0);
 		       
-      else if (IsOper(target_p))
+      else
+#endif
+       if (IsOper(target_p))
 	sendto_one(source_p, form_str(RPL_TRACEOPERATOR),
 		   me.name, source_p->name, class_name, name, 
 #ifndef HIDE_SPOOF_IPS
@@ -318,13 +292,16 @@ static int report_this_status(struct Client *source_p, struct Client *target_p,
 		   (target_p->user)?(CurrentTime - target_p->user->last):0);
 
       break;
-    case STAT_SERVER:
-      if(!IsOperAdmin(source_p))
-        name = get_client_name(target_p, MASK_IP);
 
+    case STAT_SERVER:
       sendto_one(source_p, form_str(RPL_TRACESERVER),
 		 me.name, source_p->name, class_name, link_s_p,
-		 link_u_p, name, *(target_p->serv->by) ?
+		 link_u_p, 
+#ifndef HIDE_SPOOF_IPS
+                 IsOperAdmin(source_p) ? name :
+#endif
+                 target_p->name,
+                 *(target_p->serv->by) ?
 		 target_p->serv->by : "*", "*",
 		 me.name, CurrentTime - target_p->lasttime);
       break;
