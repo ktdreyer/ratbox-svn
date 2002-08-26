564f5897a8150384ffbebb34451f96e5a28c8c9f
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-08-26 02:17:59 +0000

    - if anonops are compiled out, dont show the option.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@12525 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_info.c
+++ modules/m_info.c
@@ -420,12 +420,14 @@ static struct InfoStruct info_table[] =
     &ConfigChannel.quiet_on_ban,
     "Banned users may not send text to a channel"
   },
+#ifdef ANONOPS
   {
     "use_anonops",
     OUTPUT_BOOLEAN_YN,
     &ConfigChannel.use_anonops,
     "Enable chanmode +a (anonymous ops)",
   },
+#endif
   {
     "use_except",
     OUTPUT_BOOLEAN_YN,
