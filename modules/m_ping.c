6b386c8a2ebd98979a8afe59ef567c38b8b46563
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-10-12 19:14:59 +0000

    Fix from hybrid-7:
      - I hate server hostmasking. I really hate it.
        In any case, the match() in m_ping.c should have been an irccmp()
        ping is looking for an exact match on "me.name" not a wild card match.
    
    
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@12812 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_ping.c
+++ modules/m_ping.c
@@ -89,7 +89,7 @@ static void m_ping(struct Client *client_p,
    return;
   }
 
-  if (!EmptyString(destination) && !match(destination, me.name))
+  if (!EmptyString(destination) && !irccmp(destination, me.name))
   {
     /* We're sending it across servers.. origin == client_p->name --fl_ */
     origin = client_p->name;
@@ -144,7 +144,7 @@ static void ms_ping(struct Client *client_p,
     origin = client_p->name;
 #endif
 
-  if (!EmptyString(destination) && !match(destination, me.name))
+  if (!EmptyString(destination) && !irccmp(destination, me.name))
   {
     if ((target_p = find_server(destination)))
       sendto_one(target_p,":%s PING %s :%s", parv[0],
