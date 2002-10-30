fcd2bcdba450fdb3d0936ce4ea199708d5967792
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-10-30 02:01:20 +0000

    Yet another fixup..
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@12923 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/s_bsd.c
+++ src/s_bsd.c
@@ -384,19 +384,18 @@ void add_connection(struct Listener* listener, int fd)
 #else
   new_client->localClient->aftype = AF_INET;
 #endif
-
+  *new_client->host = '\0';
 #ifdef IPV6
   if(*new_client->localClient->sockhost == ':')
-  {
     strlcat(new_client->host, "0",HOSTLEN+1);
-  }
+
   if(new_client->localClient->aftype == AF_INET6 && ConfigFileEntry.dot_in_ip6_addr == 1)
   {
-    strlcat(new_client->host, new_client->localClient->sockhost,sizeof(*new_client->host));
-    strlcat(new_client->host, ".",sizeof(*new_client->host));
+    strlcat(new_client->host, new_client->localClient->sockhost,HOSTLEN+1);
+    strlcat(new_client->host, ".",HOSTLEN+1);
   } else
 #endif
-    strlcat(new_client->host, new_client->localClient->sockhost,sizeof(*new_client->host));
+    strlcat(new_client->host, new_client->localClient->sockhost,HOSTLEN+1);
 
   new_client->localClient->fd        = fd;
   
