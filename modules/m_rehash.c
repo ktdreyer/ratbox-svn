9c7158f945bbf6dc8998506f1620b7bfa9b924af
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-11-28 22:27:12 +0000

    Reworked REHASH to use a table, instead of the maze of if()'s
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@13235 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_rehash.c
+++ modules/m_rehash.c
@@ -43,8 +43,6 @@
 #include "hostmask.h"
 
 static void mo_rehash(struct Client*, struct Client*, int, char**);
-static void clear_temps(dlink_list *);
-static void clear_pending_glines(void);
 
 struct Message rehash_msgtab = {
   "REHASH", 0, 0, 0, 0, MFLG_SLOW, 0,
@@ -66,108 +64,13 @@ _moddeinit(void)
 
 const char *_version = "$Revision$";
 #endif
-/*
- * mo_rehash - REHASH message handler
- *
- */
-static void mo_rehash(struct Client *client_p, struct Client *source_p,
-                     int parc, char *parv[])
-{
-  if (!IsOperRehash(source_p))
-    {
-      sendto_one(source_p,":%s NOTICE %s :You need rehash = yes;", me.name, parv[0]);
-      return;
-    }
 
-  if (parc > 1)
-    {
-      if (irccmp(parv[1],"CHANNELS") == 0)
-        {
-          sendto_realops_flags(UMODE_ALL, L_ALL,
-                       "%s is forcing cleanup of channels",parv[0]);
-	  cleanup_channels(NULL);
-        }
-      else if(irccmp(parv[1],"DNS") == 0)
-        {
-          sendto_one(source_p, form_str(RPL_REHASHING), me.name, parv[0], "DNS");
-          sendto_realops_flags(UMODE_ALL, L_ALL,"%s is rehashing DNS",
-                               get_oper_name(source_p));
-          restart_resolver();   /* re-read /etc/resolv.conf AGAIN?
-                                   and close/re-open res socket */
-        }
-      else if(irccmp(parv[1],"MOTD") == 0)
-        {
-          sendto_realops_flags(UMODE_ALL, L_ALL,
-		       "%s is forcing re-reading of MOTD file",
-		       get_oper_name(source_p));
-          ReadMessageFile( &ConfigFileEntry.motd );
-        }
-      else if(irccmp(parv[1],"OMOTD") == 0)
-        {
-          sendto_realops_flags(UMODE_ALL, L_ALL,
-		       "%s is forcing re-reading of OPER MOTD file",
-		       get_oper_name(source_p));
-          ReadMessageFile( &ConfigFileEntry.opermotd );
-        }
-      else if(irccmp(parv[1], "GLINES") == 0)
-      {
-        sendto_realops_flags(UMODE_ALL, L_ALL,
-                             "%s is clearing G-lines",
-                             source_p->name);
-        clear_temps(&glines);
-      }
-      else if(irccmp(parv[1], "PGLINES") == 0)
-      {
-        sendto_realops_flags(UMODE_ALL, L_ALL,
-                             "%s is clearing pending glines",
-                             source_p->name);
-        clear_pending_glines();
-      }
-      else if(irccmp(parv[1], "TKLINES") == 0)
-      {
-        sendto_realops_flags(UMODE_ALL, L_ALL,
-                             "%s is clearing temp klines",
-                             source_p->name);
-        clear_temps(&tkline_min);
-        clear_temps(&tkline_hour);
-        clear_temps(&tkline_day);
-        clear_temps(&tkline_week);
-      }
-      else if(irccmp(parv[1], "TDLINES") == 0)
-      {
-        sendto_realops_flags(UMODE_ALL, L_ALL,
-                             "%s is clearing temp dlines",
-                             source_p->name);
-        clear_temps(&tdline_min);
-        clear_temps(&tdline_hour);
-        clear_temps(&tdline_day);
-        clear_temps(&tdline_week);
-      }
-      else
-      {
-        sendto_one(source_p,":%s NOTICE %s :rehash one of: "
-                   "GLINES PGLINES TKLINES TDLINES CHANNELS DNS MOTD OMOTD",
-                   me.name, source_p->name);
-        return;
-      }
-         
-      ilog(L_NOTICE, "REHASH %s From %s", parv[1], 
-           get_client_name(source_p, HIDE_IP));
-      return;
-    }
-  else
-    {
-      sendto_one(source_p, form_str(RPL_REHASHING), me.name, parv[0],
-                 ConfigFileEntry.configfile);
-      sendto_realops_flags(UMODE_ALL, L_ALL,
-			   "%s is rehashing server config file",
-			   get_oper_name(source_p));
-      ilog(L_NOTICE, "REHASH From %s[%s]", get_oper_name(source_p),
-           source_p->localClient->sockhost);
-      rehash(0);
-      return;
-    }
-}
+struct hash_commands
+{
+	const char *cmd;
+	void (*handler)(struct Client *source_p);
+};
+
 
 static void
 clear_temps(dlink_list *tlist)
@@ -200,6 +103,143 @@ clear_pending_glines(void)
     MyFree(glp_ptr->reason2);
     MyFree(glp_ptr);
     dlinkDestroy(ptr, &pending_glines);
+  }
+}
+
+
+
+static void rehash_channels(struct Client *source_p)
+{
+   sendto_realops_flags(UMODE_ALL, L_ALL,
+                        "%s is forcing cleanup of channels", source_p->name);
+   cleanup_channels(NULL);
+}
+
+static void rehash_dns(struct Client *source_p)
+{
+   sendto_realops_flags(UMODE_ALL, L_ALL,"%s is rehashing DNS",
+                               get_oper_name(source_p));
+   restart_resolver();   /* re-read /etc/resolv.conf AGAIN?
+                            and close/re-open res socket */
+}
+
+static void rehash_motd(struct Client *source_p)
+{
+   sendto_realops_flags(UMODE_ALL, L_ALL,
+		       "%s is forcing re-reading of MOTD file",
+		       get_oper_name(source_p));
+   ReadMessageFile( &ConfigFileEntry.motd );
+}
+
+static void rehash_omotd(struct Client *source_p)
+{
+   sendto_realops_flags(UMODE_ALL, L_ALL,
+		       "%s is forcing re-reading of OPER MOTD file",
+		       get_oper_name(source_p));
+   ReadMessageFile( &ConfigFileEntry.opermotd );
+}
+
+static void rehash_glines(struct Client *source_p)
+{
+   sendto_realops_flags(UMODE_ALL, L_ALL,
+                        "%s is clearing G-lines",
+                        source_p->name);
+   clear_temps(&glines);
+}
+
+static void rehash_pglines(struct Client *source_p)
+{
+   sendto_realops_flags(UMODE_ALL, L_ALL,
+                        "%s is clearing pending glines",
+                        source_p->name);
+   clear_pending_glines();
+}
+
+static void rehash_tklines(struct Client *source_p)
+{
+   sendto_realops_flags(UMODE_ALL, L_ALL,
+                        "%s is clearing temp klines",
+                         source_p->name);
+   clear_temps(&tkline_min);
+   clear_temps(&tkline_hour);
+   clear_temps(&tkline_day);
+   clear_temps(&tkline_week);
+}
+
+static void rehash_tdlines(struct Client *source_p)
+{
+   sendto_realops_flags(UMODE_ALL, L_ALL,
+                         "%s is clearing temp dlines",
+                         source_p->name);
+   clear_temps(&tdline_min);
+   clear_temps(&tdline_hour);
+   clear_temps(&tdline_day);
+   clear_temps(&tdline_week);
+}
+
+static struct hash_commands rehash_commands[] =
+{
+	{ "CHANNELS", rehash_channels },
+	{ "DNS", rehash_dns },
+	{ "MOTD", rehash_motd },
+	{ "OMOTD", rehash_omotd },
+	{ "GLINES", rehash_glines },
+	{ "PGLINES", rehash_pglines },
+	{ "TKLINES", rehash_tklines },
+	{ "TDLINES", rehash_tdlines },
+	{ NULL, NULL }
+};
+
+/*
+ * mo_rehash - REHASH message handler
+ *
+ */
+static void mo_rehash(struct Client *client_p, struct Client *source_p,
+                     int parc, char *parv[])
+{
+  if (!IsOperRehash(source_p))
+  {
+      sendto_one(source_p,":%s NOTICE %s :You need rehash = yes;", me.name, parv[0]);
+      return;
+  }
+
+  if (parc > 1)
+  {
+      int x;
+      char cmdbuf[100];
+      
+      for(x = 0; rehash_commands[x].cmd != NULL && rehash_commands[x].handler != NULL; x++)
+      {
+         if(irccmp(parv[1], rehash_commands[x].cmd) == 0)
+         {
+            sendto_one(source_p, form_str(RPL_REHASHING), me.name, source_p->name, rehash_commands[x].cmd);
+            rehash_commands[x].handler(source_p);
+            ilog(L_NOTICE, "REHASH %s From %s[%s]", parv[1], get_oper_name(source_p), 
+                            source_p->localClient->sockhost);
+	    return;           
+         } 
+      }
+      
+      /* We are still here..we didn't match */
+      cmdbuf[0] = '\0';
+      for(x = 0; rehash_commands[x].cmd != NULL && rehash_commands[x].handler != NULL; x++)
+      {
+           strlcat(cmdbuf, " ", sizeof(cmdbuf));
+           strlcat(cmdbuf, rehash_commands[x].cmd, sizeof(cmdbuf));
+      }
+      sendto_one(source_p, ":%s NOTICE %s :rehash one of:%s", me.name, source_p->name, cmdbuf);       
+  }
+  else
+  {
+      sendto_one(source_p, form_str(RPL_REHASHING), me.name, parv[0],
+                 ConfigFileEntry.configfile);
+      sendto_realops_flags(UMODE_ALL, L_ALL,
+			   "%s is rehashing server config file",
+			   get_oper_name(source_p));
+      ilog(L_NOTICE, "REHASH From %s[%s]", get_oper_name(source_p),
+           source_p->localClient->sockhost);
+      rehash(0);
+      return;
   }
 }
 
