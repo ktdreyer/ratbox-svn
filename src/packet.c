c5b1006ed3bb81dba93300e1d1237832158522a9
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2002-12-10 20:43:01 +0000

    Add another MyConnect() check
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@13413 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/packet.c
+++ src/packet.c
@@ -99,9 +99,10 @@ parse_client_queued(struct Client *client_p)
 
 	if(IsAnyServer(client_p) || IsExemptFlood(client_p))
 	{
-		while ((dolen =
-			linebuf_get(&client_p->localClient->buf_recvq,
-				    readBuf, READBUF_SIZE, LINEBUF_COMPLETE, LINEBUF_PARSED)) > 0)
+		while (MyConnect(client_p) && (dolen =
+					       linebuf_get(&client_p->localClient->buf_recvq,
+							   readBuf, READBUF_SIZE, LINEBUF_COMPLETE,
+							   LINEBUF_PARSED)) > 0)
 		{
 			if(!IsDead(client_p))
 				client_dopacket(client_p, readBuf, dolen);
