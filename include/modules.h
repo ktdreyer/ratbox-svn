bdd15b11166d715ea7d1542b990506634512823a
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2003-02-08 23:56:31 +0000

    Accidently commited some test code
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@13773 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- include/modules.h
+++ include/modules.h
@@ -96,7 +96,7 @@ struct mapi_mheader_av1
 	struct mapi_mheader_av1 _mheader = { MAPI_V1, reg, unreg, cl, hl, hfnlist, v}
 #else
 # define DECLARE_MODULE_AV1(reg,unreg,cl,hl,hfnlist, v) \
-	struct mapi_mheader_av1 MODNAME ##_mheader = { MAPI_V1, reg, unreg, cl, hl, hfnlist, v}
+	struct mapi_mheader_av1 MODNAME  = { MAPI_V1, reg, unreg, cl, hl, hfnlist, v}
 void load_static_modules();
 #endif
 
