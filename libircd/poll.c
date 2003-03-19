3545ebe526a33bf0d461fd44874246b46ce265a2
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2003-03-19 07:20:37 +0000

    Somehow i goofed up s_bsd_poll.c revert back to the version in stable.
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@13887 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- libircd/poll.c
+++ libircd/poll.c
@@ -1,5 +1,5 @@
 /*
- *  ircd-ratbox: A slightly useful ircd.
+ *  ircd-ratbox: an advanced Internet Relay Chat Daemon(ircd).
  *  s_bsd_poll.c: POSIX poll() compatible network routines.
  *
  *  Copyright (C) 1990 Jarkko Oikarinen and University of Oulu, Co Center
@@ -60,15 +60,40 @@
 #define POLLWRNORM POLLOUT
 #endif
 
+struct _pollfd_list
+{
+	struct pollfd pollfds[MAXCONNECTIONS];
+	int maxindex;		/* highest FD number */
+};
 
-static struct pollfd *pfds;
-static int used_count = 0;
-static fde_t **index_to_fde;
+typedef struct _pollfd_list pollfd_list_t;
 
+pollfd_list_t pollfd_list;
 static void poll_update_pollfds(int, short, PF *);
 
 /* XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX */
 /* Private functions */
+
+/*
+ * find a spare slot in the fd list. We can optimise this out later!
+ *   -- adrian
+ */
+static inline int
+poll_findslot(void)
+{
+	int i;
+	for (i = 0; i < MAXCONNECTIONS; i++)
+	{
+		if(pollfd_list.pollfds[i].fd == -1)
+		{
+			/* MATCH!!#$*&$ */
+			return i;
+		}
+	}
+	assert(1 == 0);
+	/* NOTREACHED */
+	return -1;
+}
 
 /*
  * set and clear entries in the pollfds[] array.
@@ -77,45 +102,41 @@ static void
 poll_update_pollfds(int fd, short event, PF * handler)
 {
 	fde_t *F = &fd_table[fd];
-	struct pollfd *pf;
 	int comm_index;
 
 	if(F->comm_index < 0)
 	{
-		used_count++;
-		F->comm_index = used_count - 1;
-		index_to_fde[F->comm_index] = F;
+		F->comm_index = poll_findslot();
 	}
 	comm_index = F->comm_index;
 
-	pf = &pfds[comm_index];
-
-
 	/* Update the events */
-	if(handler != NULL)
+	if(handler)
 	{
 		F->list = FDLIST_IDLECLIENT;
-		pf->events |= event;
-		pf->fd = fd;
+		pollfd_list.pollfds[comm_index].events |= event;
+		pollfd_list.pollfds[comm_index].fd = fd;
+		/* update maxindex here */
+		if(comm_index > pollfd_list.maxindex)
+			pollfd_list.maxindex = comm_index;
 	}
 	else
 	{
 		if(comm_index >= 0)
 		{
-			pf->events &= ~event;
-			if(pf->events == 0)
+			pollfd_list.pollfds[comm_index].events &= ~event;
+			if(pollfd_list.pollfds[comm_index].events == 0)
 			{
-				pf->fd = -1;
-				pf->revents = 0;
-				if(comm_index != used_count - 1)
-				{
-					index_to_fde[used_count - 1]->comm_index = comm_index;
-					pfds[comm_index] = pfds[used_count - 1];
-				}
-				pfds[used_count - 1].fd = -1;
+				pollfd_list.pollfds[comm_index].fd = -1;
+				pollfd_list.pollfds[comm_index].revents = 0;
 				F->comm_index = -1;
 				F->list = FDLIST_NONE;
-				used_count--;
+
+				/* update pollfd_list.maxindex here */
+				if(comm_index == pollfd_list.maxindex)
+					while (pollfd_list.maxindex >= 0 &&
+					       pollfd_list.pollfds[pollfd_list.maxindex].fd == -1)
+						pollfd_list.maxindex--;
 			}
 		}
 	}
@@ -135,8 +156,13 @@ poll_update_pollfds(int fd, short event, PF * handler)
 void
 init_netio(void)
 {
-	pfds = MyMalloc(MAXCONNECTIONS * sizeof(struct pollfd));
-	index_to_fde = MyMalloc(MAXCONNECTIONS * sizeof(fde_t *));
+	int fd;
+
+	for (fd = 0; fd < MAXCONNECTIONS; fd++)
+	{
+		pollfd_list.pollfds[fd].fd = -1;
+	}
+	pollfd_list.maxindex = 0;
 }
 
 /*
@@ -188,13 +214,12 @@ comm_select(unsigned long delay)
 	int num;
 	int fd;
 	int ci;
-	int rused;
 	PF *hdl;
 
 	for (;;)
 	{
 		/* XXX kill that +1 later ! -- adrian */
-		num = poll(pfds, used_count, delay);
+		num = poll(pollfd_list.pollfds, pollfd_list.maxindex + 1, delay);
 		if(num >= 0)
 			break;
 		if(ignoreErrno(errno))
@@ -208,44 +233,35 @@ comm_select(unsigned long delay)
 	/* update current time again, eww.. */
 	set_time();
 	callbacks_called += num;
+
 	if(num == 0)
 		return 0;
-	rused = used_count;	/* Must safe this here..used_count can fall while looping */
-
 	/* XXX we *could* optimise by falling out after doing num fds ... */
-	for (ci = 0; ci < rused; ci++)
+	for (ci = 0; ci < pollfd_list.maxindex + 1; ci++)
 	{
 		fde_t *F;
 		int revents;
-		if(((revents = pfds[ci].revents) == 0) || (pfds[ci].fd) == -1)
+		if(((revents = pollfd_list.pollfds[ci].revents) == 0) ||
+		   (pollfd_list.pollfds[ci].fd) == -1)
 			continue;
-		fd = pfds[ci].fd;
+		fd = pollfd_list.pollfds[ci].fd;
 		F = &fd_table[fd];
 		if(revents & (POLLRDNORM | POLLIN | POLLHUP | POLLERR))
 		{
 			hdl = F->read_handler;
 			F->read_handler = NULL;
+			poll_update_pollfds(fd, POLLRDNORM, NULL);
 			if(hdl)
 				hdl(fd, F->read_data);
-
 		}
-
-		if(F->flags.open == 0)
-			continue;	/* Read handler closed us..go on */
 		if(revents & (POLLWRNORM | POLLOUT | POLLHUP | POLLERR))
 		{
 			hdl = F->write_handler;
 			F->write_handler = NULL;
+			poll_update_pollfds(fd, POLLWRNORM, NULL);
 			if(hdl)
 				hdl(fd, F->write_data);
-
 		}
-
-		if(F->read_handler == NULL)
-			poll_update_pollfds(fd, POLLRDNORM, NULL);
-		if(F->write_handler == NULL)
-			poll_update_pollfds(fd, POLLWRNORM, NULL);
-
 	}
 	return 0;
 }
