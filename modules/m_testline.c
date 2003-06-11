5e3ac9db64281410bb34df5ee63dd800c0a7c3ce
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2003-06-11 08:30:54 +0000

    - revert the testline changes
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@14095 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_testline.c
+++ modules/m_testline.c
@@ -72,7 +72,6 @@ mo_testline(struct Client *client_p, struct Client *source_p, int parc, const ch
 	struct sockaddr_storage ip;
 	int host_mask, t;
 	char *host, *pass, *user, *name, *classname, *given_host, *given_name, *p;
-	char *oper_reason;
 	int port;
 
 	if(parc > 1)
@@ -91,7 +90,7 @@ mo_testline(struct Client *client_p, struct Client *source_p, int parc, const ch
 				aconf = find_dline(&ip,t);
 				if(aconf)
 				{
-					get_printable_kline(source_p, aconf, &host, &pass, &user, &oper_reason);
+					get_printable_conf(aconf, &name, &host, &pass, &user, &port, &classname);
 					if(aconf->status & CONF_EXEMPTDLINE)
 					{
 						sendto_one(source_p,
@@ -135,7 +134,7 @@ mo_testline(struct Client *client_p, struct Client *source_p, int parc, const ch
 
 		if(aconf)
 		{
-			get_printable_kline(source_p, aconf, &host, &pass, &user, &oper_reason);
+			get_printable_conf(aconf, &name, &host, &pass, &user, &port, &classname);
 
 			if(aconf->status & CONF_KILL)
 			{
