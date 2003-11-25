cfca3c20d1bb49ad73ae5276c00b4ad58055216f
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2003-11-25 15:48:11 +0000

    Lets try that again
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@15441 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/sigio.c
+++ src/sigio.c
@@ -237,8 +237,8 @@ comm_select(unsigned long delay)
 	fde_t *F;
 	struct siginfo si;
 	struct timespec timeout;
-	timeout.tv_sec = (delay / 1000000);
-	timeout.tv_nsec = (delay % 1000000) * delay;
+	timeout.tv_sec = (delay / 1000);
+	timeout.tv_nsec = (delay % 1000) * 1000000;
 	for (;;)
 	{
 		if(!sigio_is_screwed)
