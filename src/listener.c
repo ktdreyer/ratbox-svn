a5dc7bf55bc999700d4bcb5fe1f02910341937b3
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2004-03-01 18:39:42 +0000

    Fix a compile error
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@15732 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/listener.c
+++ src/listener.c
@@ -64,12 +64,12 @@ make_listener(struct sockaddr_storage *addr)
 	listener->name = me.name;
 	listener->fd = -1;
 	memcpy(&listener->addr, addr, sizeof(struct sockaddr_storage));
-	if(listener->addr->ss_family == AF_INET)
+	if(listener->addr.ss_family == AF_INET)
 	{
 		SET_SS_LEN(listener->addr, sizeof(struct sockaddr_in));
 	}
 #ifdef IPV6
-	 else if(listener->addr->ss_family == AF_INET6)
+	 else if(listener->addr.ss_family == AF_INET6)
 	{
 		SET_SS_LEN(listener->addr, sizeof(struct sockaddr_in6));
 	}
