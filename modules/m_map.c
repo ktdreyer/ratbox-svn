fb4ec735c35728a06dbe8b5b8bbd44e11cc9caaa
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2004-03-03 22:07:45 +0000

    - hwys patch to add SIDs to /map - replace a strncat() with a strlcat()
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@15882 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_map.c
+++ modules/m_map.c
@@ -90,7 +90,13 @@ dump_map(struct Client *client_p, struct Client *root_p, char *pbuf)
 	dlink_node *ptr;
 	*pbuf = '\0';
 
-	strncat(pbuf, root_p->name, BUFSIZE - ((size_t) pbuf - (size_t) buf));
+	strlcat(pbuf, root_p->name, BUFSIZE);
+	if (has_id(root_p))
+	{
+		strlcat(pbuf, "[", BUFSIZE);
+		strlcat(pbuf, root_p->id, BUFSIZE);
+		strlcat(pbuf, "]", BUFSIZE);
+	}
 	len = strlen(buf);
 	buf[len] = ' ';
 
