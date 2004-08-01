a7793a2ad773c3674ecb2c6c607cc970d78c5ef9
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2004-08-01 00:05:20 +0000

    - merge tstats() into m_stats.c
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@17329 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_stats.c
+++ modules/m_stats.c
@@ -50,6 +50,7 @@
 #include "hook.h"
 #include "s_newconf.h"
 #include "hash.h"
+#include "reject.h"
 
 static int m_stats (struct Client *, struct Client *, int, const char **);
 
@@ -907,7 +908,99 @@ stats_usage (struct Client *source_p)
 static void
 stats_tstats (struct Client *source_p)
 {
-	tstats (source_p);
+	struct Client *target_p;
+	struct ServerStatistics sp;
+	dlink_node *ptr;
+
+	memcpy(&sp, &ServerStats, sizeof(struct ServerStatistics));
+
+	DLINK_FOREACH(ptr, serv_list.head)
+	{
+		target_p = ptr->data;
+
+		sp.is_sbs += target_p->localClient->sendB;
+		sp.is_sbr += target_p->localClient->receiveB;
+		sp.is_sks += target_p->localClient->sendK;
+		sp.is_skr += target_p->localClient->receiveK;
+		sp.is_sti += CurrentTime - target_p->firsttime;
+		sp.is_sv++;
+
+		if(sp.is_sbs > 1023)
+		{
+			sp.is_sks += (sp.is_sbs >> 10);
+			sp.is_sbs &= 0x3ff;
+		}
+		if(sp.is_sbr > 1023)
+		{
+			sp.is_skr += (sp.is_sbr >> 10);
+			sp.is_sbr &= 0x3ff;
+		}
+	}
+
+	DLINK_FOREACH(ptr, lclient_list.head)
+	{
+		target_p = ptr->data;
+
+		sp.is_cbs += target_p->localClient->sendB;
+		sp.is_cbr += target_p->localClient->receiveB;
+		sp.is_cks += target_p->localClient->sendK;
+		sp.is_ckr += target_p->localClient->receiveK;
+		sp.is_cti += CurrentTime - target_p->firsttime;
+		sp.is_cl++;
+		if(sp.is_cbs > 1023)
+		{
+			sp.is_cks += (sp.is_cbs >> 10);
+			sp.is_cbs &= 0x3ff;
+		}
+		if(sp.is_cbr > 1023)
+		{
+			sp.is_ckr += (sp.is_cbr >> 10);
+			sp.is_cbr &= 0x3ff;
+		}
+
+	}
+
+	DLINK_FOREACH(ptr, unknown_list.head)
+	{
+		sp.is_ni++;
+	}
+
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :accepts %u refused %u", sp.is_ac, sp.is_ref);
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			"T :rejected %u delaying %lu", 
+			sp.is_rej, dlink_list_length(&delay_exit));
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			"T :nicks being delayed %lu",
+			get_nd_count());
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :unknown commands %u prefixes %u",
+			   sp.is_unco, sp.is_unpf);
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :nick collisions %u unknown closes %u",
+			   sp.is_kill, sp.is_ni);
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :wrong direction %u empty %u", 
+			   sp.is_wrdi, sp.is_empt);
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :numerics seen %u", sp.is_num);
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :auth successes %u fails %u",
+			   sp.is_asuc, sp.is_abad);
+	sendto_one_numeric(source_p, RPL_STATSDEBUG, "T :Client Server");
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :connected %u %u", sp.is_cl, sp.is_sv);
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :bytes sent %d.%uK %d.%uK",
+			   (int) sp.is_cks, sp.is_cbs, 
+			   (int) sp.is_sks, sp.is_sbs);
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :bytes recv %d.%uK %d.%uK",
+			   (int) sp.is_ckr, sp.is_cbr, 
+			   (int) sp.is_skr, sp.is_sbr);
+	sendto_one_numeric(source_p, RPL_STATSDEBUG,
+			   "T :time connected %d %d",
+			   (int) sp.is_cti, (int) sp.is_sti);
 }
 
 static void
