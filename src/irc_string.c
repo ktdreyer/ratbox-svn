5cf10765fe9ccc3055edaea43f28ed03b49f1531
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2004-08-09 16:53:56 +0000

    Add slightly better strlcpy/strlcat from the linux kernel
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@17473 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/irc_string.c
+++ src/irc_string.c
@@ -697,94 +697,41 @@ inetpton(af, src, dst)
 	/* NOTREACHED */
 }
 
-/*
- * strlcat and strlcpy were ripped from openssh 2.5.1p2
- * They had the following Copyright info: 
- *
- *
- * Copyright (c) 1998 Todd C. Miller <Todd.Miller@courtesan.com>
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright    
- *    notice, this list of conditions and the following disclaimer in the  
- *    documentation and/or other materials provided with the distribution. 
- * 3. The name of the author may not be used to endorse or promote products
- *    derived from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED `AS IS'' AND ANY EXPRESS OR IMPLIED WARRANTIES,
- * INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
- * AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL
- * THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
- * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
- * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
- * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+/* The strlcat/strlcpy below are from the linux kernel
+ * source tree, these appear to be faster than the ones
+ * that used to be here. The following (C) was found on these
+ * Copyright (C) 1991, 1992  Linus Torvalds
  */
-
-
 
 #ifndef HAVE_STRLCAT
 size_t
-strlcat(char *dst, const char *src, size_t siz)
+strlcat(char *dest, const char *src, size_t count)
 {
-	char *d = dst;
-	const char *s = src;
-	size_t n = siz, dlen;
+        size_t dsize = strlen(dest);
+        size_t len = strlen(src);
+        size_t res = dsize + len;
 
-	while (n-- != 0 && *d != '\0')
-		d++;
-	dlen = d - dst;
-	n = siz - dlen;
-
-	if(n == 0)
-		return (dlen + strlen(s));
-	while (*s != '\0')
-	{
-		if(n != 1)
-		{
-			*d++ = *s;
-			n--;
-		}
-		s++;
-	}
-	*d = '\0';
-	return (dlen + (s - src));	/* count does not include NUL */
+        dest += dsize;   
+        count -= dsize;  
+        if (len >= count)
+                len = count-1; 
+        memcpy(dest, src, len);
+        dest[len] = 0;
+        return res;
 }
 #endif
 
 #ifndef HAVE_STRLCPY
 size_t
-strlcpy(char *dst, const char *src, size_t siz)
+strlcpy(char *dest, const char *src, size_t size)
 {
-	char *d = dst;
-	const char *s = src;
-	size_t n = siz;
-	/* Copy as many bytes as will fit */
-	if(n != 0 && --n != 0)
-	{
-		do
-		{
-			if((*d++ = *s++) == 0)
-				break;
-		}
-		while (--n != 0);
-	}
-	/* Not enough room in dst, add NUL and traverse rest of src */
-	if(n == 0)
-	{
-		if(siz != 0)
-			*d = '\0';	/* NUL-terminate dst */
-		while (*s++)
-			;
-	}
+        size_t ret = strlen(src);
 
-	return (s - src - 1);	/* count does not include NUL */
+        if (size) {
+                size_t len = (ret >= size) ? size-1 : ret;
+                memcpy(dest, src, len);
+                dest[len] = '\0';
+        }
+        return ret;
 }
 #endif
