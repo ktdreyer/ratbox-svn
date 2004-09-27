593b5f640dc32bdce71d9437ef1c96472d650509
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2004-09-27 14:16:52 +0000

    shut up gcc
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@17894 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/irc_string.c
+++ src/irc_string.c
@@ -469,8 +469,8 @@ inetntop(int af, const void *src, char *dst, unsigned int size)
 		if(IN6_IS_ADDR_V4MAPPED((const struct in6_addr *) src) ||
 		   IN6_IS_ADDR_V4COMPAT((const struct in6_addr *) src))
 			return (inet_ntop4
-				((unsigned char *)
-				 &((struct in6_addr *) src)->s6_addr[12], dst, size));
+				((const unsigned char *)
+				 &((const struct in6_addr *) src)->s6_addr[12], dst, size));
 		else
 			return (inet_ntop6(src, dst, size));
 
