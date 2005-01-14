6f78c58d239982bc96726b44f4d41289c0ae20de
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-01-14 18:10:39 +0000

    Make watch throttle, don't accept letter commands stacked
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@18211 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/m_watch.c
+++ modules/m_watch.c
@@ -78,152 +78,156 @@ int
 m_watch(struct Client * client_p, struct Client * source_p, int parc, const char **parv)
 {
 	struct Client *aclient_p;
+	static time_t last_used = 0;
 	char *s, *p, *user;
 	char def[2] = "l";
 	char *tmp;
 	int x;
+
+
+
 	if(parc < 2)
+		s = def;
+	else
+		s = LOCAL_COPY(parv[1]);
+
+
+	if((last_used + ConfigFileEntry.pace_wait_simple) > CurrentTime)
 	{
-		/*
-		 * Default to 'l' - list who's currently online 
-		 */
-		parc = 2;
-		parv[1] = def;
-	}
 
-	for(x = 1; x < parc; x++)
-	{ 
-		tmp = LOCAL_COPY(parv[x]);
-		for(p = NULL, s = strtoken(&p, tmp, ", "); s; s = strtoken(&p, NULL, ", "))
+                sendto_one(source_p, form_str(RPL_LOAD2HI),
+                           me.name, source_p->name, "WATCH");
+		sendto_one(source_p, form_str(RPL_ENDOFWATCHLIST),
+                	   me.name, source_p->name, "");
+		return 0;			
+	} else
+		last_used = CurrentTime;
+	
+
+	switch(*s)
+	{
+		case 'L':
+		case 'l':
 		{
-			if((user = (char *) strchr(s, '!')))
-				*user++ = '\0';	/* Not used */
-				
-			/*
-			 * Prefix of "+", they want to add a name to their WATCH
-			 * list. 
-			 */
-			if(*s == '+')
-			{
-				if(*(s + 1))
-				{
-					if(dlink_list_length(&source_p->localClient->watchlist) >= ConfigFileEntry.max_watch)
-					{
-						sendto_one(source_p, form_str(ERR_TOOMANYWATCH), me.name, client_p->name, s + 1, ConfigFileEntry.max_watch);
-						continue;
-					}
-					add_to_watch_hash_table(s + 1, source_p);
-				}
-				show_watch(source_p, s + 1, RPL_NOWON, RPL_NOWOFF);
-				continue;
-			}
-			
-			/*
-			 * Prefix of "-", coward wants to remove somebody from their
-			 * WATCH list.  So do it. :-)
-			 */
-			if(*s == '-')
-			{
-				del_from_watch_hash_table(s + 1, source_p);
-				show_watch(source_p, s + 1, RPL_WATCHOFF, RPL_WATCHOFF);
-				continue;
-			}
-			
-			/*
-			 * Fancy "C" or "c", they want to nuke their WATCH list and start
-			 * over, so be it.
-			 */
-			if(*s == 'C' || *s == 'c')
-			{
-				hash_del_watch_list(source_p);
-				continue;
-			}
-			
-			/*
-			 * Now comes the fun stuff, "S" or "s" returns a status report of
-			 * their WATCH list.  I imagine this could be CPU intensive if its
-			 * done alot, perhaps an auto-lag on this?
-			 */
-			if(*s == 'S' || *s == 's')
-			{
-				dlink_node *lp;
-				struct Watch *awptr;
-				int count = 0;
-				
-				/*
-				 * Send a list of how many users they have on their WATCH list
-				 * and how many WATCH lists they are on.
-				 */
-				awptr = hash_get_watch(source_p->name);
-				if(awptr) 
-					count = dlink_list_length(&awptr->watched_by);
-				
-				sendto_one(source_p, form_str(RPL_WATCHSTAT), me.name, client_p->name,
-						   dlink_list_length(&source_p->localClient->watchlist), 
-						   count);
-				/*
-				 * Send a list of everybody in their WATCH list. Be careful
-				 * not to buffer overflow.
-				 */	
-				if(dlink_list_length(&source_p->localClient->watchlist) == 0)
-				{
-					sendto_one(source_p, form_str(RPL_ENDOFWATCHLIST), me.name, client_p->name, *s);
-					continue;
-				}
-
-				*buf = '\0';
-				count = strlen(parv[0]) + strlen(me.name) + 10;
-				
-				DLINK_FOREACH(lp, source_p->localClient->watchlist.head)
-				{
-					awptr = lp->data;
-					if(count + strlen(awptr->watchnick) + 1 > BUFSIZE - 2)
-					{
-						sendto_one(source_p, form_str(RPL_WATCHLIST), me.name, client_p->name, buf);
-						*buf = '\0';
-						count = strlen(parv[0]) + strlen(me.name) + 10;
-					}
-					strcat(buf, awptr->watchnick);
-					strcat(buf, " ");
-					count += (strlen(awptr->watchnick) + 1);
-				}
-				sendto_one(source_p, form_str(RPL_WATCHLIST), me.name, client_p->name, buf);
-				sendto_one(source_p, form_str(RPL_ENDOFWATCHLIST), me.name, client_p->name, *s);
-				continue;
-			}
-
 			/*
 			 * Well that was fun, NOT.  Now they want a list of everybody in
 			 * their WATCH list AND if they are online or offline? Sheesh,
 			 * greedy arn't we?
 			 */
-			if(*s == 'L' || *s == 'l')
+			struct Watch *awptr;
+			dlink_node *lp;
+			DLINK_FOREACH(lp, source_p->localClient->watchlist.head)
 			{
-				struct Watch *awptr;
-				dlink_node *lp;
-				DLINK_FOREACH(lp, source_p->localClient->watchlist.head)
-				{
-					awptr = lp->data;
-					if((aclient_p = find_person(awptr->watchnick)))
-						sendto_one(source_p, form_str(RPL_NOWON), me.name, client_p->name, 
-							   aclient_p->name,
-							   aclient_p->username, aclient_p->host,
-							   aclient_p->tsinfo);
-					/*
-					 * But actually, only show them offline if its a capital
-					 * 'L' (full list wanted).
-					 */
-					else if(IsUpper(*s))
-						sendto_one(source_p, form_str(RPL_NOWOFF), me.name, client_p->name,
-								   awptr->watchnick, "*",
-								   "*", awptr->lasttime);
-				}
-				sendto_one(source_p, form_str(RPL_ENDOFWATCHLIST), me.name, client_p->name, *s);
-				continue;
+				awptr = lp->data;
+				if((aclient_p = find_person(awptr->watchnick)))
+					sendto_one(source_p, form_str(RPL_NOWON), me.name, client_p->name, 
+						   aclient_p->name,
+						   aclient_p->username, aclient_p->host,
+						   aclient_p->tsinfo);
+				/*
+				 * But actually, only show them offline if its a capital
+				 * 'L' (full list wanted).
+				 */
+				else if(IsUpper(*s))
+					sendto_one(source_p, form_str(RPL_NOWOFF), me.name, client_p->name,
+							   awptr->watchnick, "*",
+							   "*", awptr->lasttime);
 			}
+			sendto_one(source_p, form_str(RPL_ENDOFWATCHLIST), me.name, client_p->name, *s);
+			return 0;
+		} 
+		case 'C':
+		case 'c':
+			hash_del_watch_list(source_p);
+			return 0;
+
+		case 'S':
+		case 's':		
+		{
 			/*
-			 * Hmm.. unknown prefix character.. Ignore it. :-) 
+			 * Now comes the fun stuff, "S" or "s" returns a status report of
+			 * their WATCH list.  I imagine this could be CPU intensive if its
+			 * done alot, perhaps an auto-lag on this?
 			 */
+			dlink_node *lp;
+			struct Watch *awptr;
+			int count = 0;
+		
+			/*
+			 * Send a list of how many users they have on their WATCH list
+			 * and how many WATCH lists they are on.
+			 */
+			awptr = hash_get_watch(source_p->name);
+			if(awptr) 
+				count = dlink_list_length(&awptr->watched_by);
+		
+			sendto_one(source_p, form_str(RPL_WATCHSTAT), me.name, client_p->name,
+					   dlink_list_length(&source_p->localClient->watchlist), 
+					   count);
+			/*
+			 * Send a list of everybody in their WATCH list. Be careful
+			 * not to buffer overflow.
+			 */	
+			if(dlink_list_length(&source_p->localClient->watchlist) == 0)
+			{
+				sendto_one(source_p, form_str(RPL_ENDOFWATCHLIST), me.name, client_p->name, *s);
+				return 0;
+			}
+
+			*buf = '\0';
+			count = strlen(parv[0]) + strlen(me.name) + 10;
+		
+			DLINK_FOREACH(lp, source_p->localClient->watchlist.head)
+			{
+				awptr = lp->data;
+				if(count + strlen(awptr->watchnick) + 1 > BUFSIZE - 2)
+				{
+					sendto_one(source_p, form_str(RPL_WATCHLIST), me.name, client_p->name, buf);
+					*buf = '\0';
+					count = strlen(parv[0]) + strlen(me.name) + 10;
+				}
+				strcat(buf, awptr->watchnick);
+				strcat(buf, " ");
+				count += (strlen(awptr->watchnick) + 1);
+			}
+			sendto_one(source_p, form_str(RPL_WATCHLIST), me.name, client_p->name, buf);
+			sendto_one(source_p, form_str(RPL_ENDOFWATCHLIST), me.name, client_p->name, *s);
+			return 0;
+		}
+		case '+':
+		case '-':
+		{
+			for(x = 1; x < parc; x++)
+			{	 
+				tmp = LOCAL_COPY(parv[x]);
+				for(p = NULL, s = strtoken(&p, tmp, ", "); s; s = strtoken(&p, NULL, ", "))
+				{
+					if((user = (char *) strchr(s, '!')))
+						*user++ = '\0';	/* Not used */
+					
+					if(*s == '+')
+					{
+						if(*(s + 1))
+						{
+							if(dlink_list_length(&source_p->localClient->watchlist) >= ConfigFileEntry.max_watch)
+							{
+								sendto_one(source_p, form_str(ERR_TOOMANYWATCH), me.name, client_p->name, s + 1, ConfigFileEntry.max_watch);
+								continue;
+							}
+							add_to_watch_hash_table(s + 1, source_p);
+						}
+						show_watch(source_p, s + 1, RPL_NOWON, RPL_NOWOFF);
+						continue;
+					} 
+					else if(*s == '-')
+					{
+						del_from_watch_hash_table(s + 1, source_p);
+						show_watch(source_p, s + 1, RPL_WATCHOFF, RPL_WATCHOFF);
+						continue;
+					} else
+						continue;
+				}
+			}			
 		}
 	}
 	return 0;
