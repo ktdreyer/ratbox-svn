c79a797fc5c7fa120ad170272fa4597e69ff9085
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-03-21 21:08:23 +0000

    - move inet_socketpair() -> m_server.c
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@18692 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- modules/core/m_server.c
+++ modules/core/m_server.c
@@ -71,6 +71,10 @@ static int set_server_gecos(struct Client *, const char *);
 
 static int server_estab(struct Client *client_p);
 
+#ifndef HAVE_SOCKETPAIR
+static int inet_socketpair(int d, int type, int protocol, int sv[2]);
+#endif
+
 /*
  * mr_server - SERVER message handler
  *      parv[0] = sender prefix
@@ -1532,4 +1536,60 @@ server_estab(struct Client *client_p)
 	return 0;
 }
 
+#ifndef HAVE_SOCKETPAIR
+static int
+inet_socketpair(int d, int type, int protocol, int sv[2])
+{
+	struct sockaddr_in addr1, addr2, addr3;
+	int addr3_len = sizeof(addr3);
+	int fd, rc;
+	int port_no = 20000;
+	
+	if(d != AF_INET || type != SOCK_STREAM || protocol)
+	{
+		errno = EAFNOSUPPORT;
+		return -1;
+	}
+	if(((sv[0] = socket(AF_INET, SOCK_STREAM, 0)) < 0) || ((sv[1] = socket(AF_INET, SOCK_STREAM, 0)) < 0))
+		return -1;
+	
+	addr1.sin_port = htons(port_no);
+	addr1.sin_family = AF_INET;
+	addr1.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	while ((rc = bind (sv[0], (struct sockaddr *) &addr1, sizeof (addr1))) < 0 && errno == EADDRINUSE)
+		addr1.sin_port = htons(++port_no);
+	
+	if(rc < 0)
+		return -1;
+	
+	if(listen(sv[0], 1) < 0)
+	{
+		close(sv[0]);
+		close(sv[1]);
+		return -1;
+	}
+	
+	addr2.sin_port = htons(port_no);
+	addr2.sin_family = AF_INET;
+	addr2.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+	if(connect (sv[1], (struct sockaddr *) &addr2, sizeof (addr2)) < 0) 
+	{
+		close(sv[0]);
+		close(sv[1]);
+		return -1;
+	}
+	
+	if((fd = accept(sv[1], (struct sockaddr *) &addr3, &addr3_len)) < 0)
+	{
+		close(sv[0]);
+		close(sv[1]);
+		return -1;
+	}
+	close(sv[0]);
+	sv[0] = fd;
+	
+	return(0);
+
+}
+#endif
 
