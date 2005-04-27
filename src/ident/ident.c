2fdb67f6307026d0bbeac473623d76fd4beac7e0
androsyn <androsyn@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2005-04-27 01:07:57 +0000

    i hate freebsd
    git-svn-id: http://svn.ratbox.org/svnroot/trunk@19073 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/ident/ident.c
+++ src/ident/ident.c
@@ -352,7 +352,7 @@ check_identd(const char *id, const char *bindaddr, const char *destaddr, const c
 
 	auth->authfd = comm_socket(((struct sockaddr *)&auth->destaddr)->sa_family, SOCK_STREAM, 0, "auth fd");
 	comm_connect_tcp(auth->authfd, (struct sockaddr *)&auth->destaddr, 
-		(struct sockaddr *)&auth->bindaddr, sizeof(auth->bindaddr), connect_callback, auth, 30);
+		(struct sockaddr *)&auth->bindaddr, GET_SS_LEN(auth->destaddr), connect_callback, auth, 30);
                                   
 }
 
