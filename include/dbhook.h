e72c52e240dd83752fbc891e8d7ed6a197152fe3
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2006-03-14 21:12:27 +0000

    - add in a dbhook api
    git-svn-id: http://svn.ratbox.org/svnroot/rserv/trunk@22147 b93f080a-4cfa-0310-a19a-80a68f531ef9

---
+++ include/dbhook.h
@@ -0,0 +1,25 @@
+#ifndef INCLUDED_dbhook_h
+#define INCLUDED_dbhook_h
+
+/* chicken and egg, these depend on each other.. */
+struct rsdb_hook;
+
+typedef int (*dbh_callback)(struct rsdb_hook *dbh, const char *value);
+
+struct rsdb_hook
+{
+	char *table;
+	char *hook_value;
+	char *field;
+	dbh_callback callback;
+	dlink_node ptr;
+};
+
+void init_rsdb_hook(void);
+
+struct rsdb_hook *rsdb_hook_add(const char *table, const char *hook_value,
+				const char *field, unsigned int frequency,
+				dbh_callback);
+void rsdb_hook_delete(struct rsdb_hook *dbh);
+
+#endif
---
+++ src/dbhook.c
@@ -0,0 +1,132 @@
+/* src/dbhook.c
+ *   Contains code for hooking into database tables.
+ *
+ * Copyright (C) 2006 Lee Hardy <leeh@leeh.co.uk>
+ * Copyright (C) 2006 ircd-ratbox development team
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * 1.Redistributions of source code must retain the above copyright notice,
+ *   this list of conditions and the following disclaimer.
+ * 2.Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * 3.The name of the author may not be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+ * INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+ * IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * $Id$
+ */
+#include "stdinc.h"
+#include "rserv.h"
+#include "tools.h"
+#include "rsdb.h"
+#include "dbhook.h"
+#include "log.h"
+#include "event.h"
+
+static void rsdb_hook_call(void *dbh);
+
+struct rsdb_hook *
+rsdb_hook_add(const char *table, const char *hook_value, const char *field, 
+		unsigned int frequency, dbh_callback callback)
+{
+	struct rsdb_hook *dbh;
+
+	if(EmptyString(table) || EmptyString(hook_value) || EmptyString(field))
+		return NULL;
+
+	dbh = my_malloc(sizeof(struct rsdb_hook));
+
+	dbh->table = my_strdup(table);
+	dbh->hook_value = my_strdup(hook_value);
+	dbh->field = my_strdup(field);
+	dbh->callback = callback;
+
+	eventAdd(hook_value, rsdb_hook_call, dbh, frequency);
+
+	return dbh;
+}
+
+void
+rsdb_hook_delete(struct rsdb_hook *dbh)
+{
+	if(dbh == NULL)
+		return;
+
+	eventDelete(rsdb_hook_call, dbh);
+
+	my_free(dbh->table);
+	my_free(dbh->hook_value);
+	my_free(dbh->field);
+
+	my_free(dbh);
+}
+
+static void
+rsdb_hook_call(void *v_dbh)
+{
+	struct rsdb_table data;
+	struct rsdb_hook *dbh = v_dbh;
+	unsigned int *delid;
+	unsigned int count;
+	int i, retval;
+
+	/* first, find how many rows we're expecting so we can allocate
+	 * memory for the ids of those to delete
+	 */
+	rsdb_exec_fetch(&data, "SELECT COUNT(id) FROM %s WHERE hook = '%Q'",
+			dbh->table, dbh->hook_value);
+	count = atoi(data.row[0][0]);
+	rsdb_exec_fetch_end(&data);
+
+	if(!count)
+		return;
+
+	delid = my_malloc(sizeof(unsigned int) * count);
+
+	/* limit our result set to count entries, as we've only allocated
+	 * memory for that many..
+	 */
+	rsdb_exec_fetch(&data, "SELECT id, %s FROM %s WHERE hook = '%Q' LIMIT %u",
+			dbh->field, dbh->table, dbh->hook_value, count);
+
+	if(data.row_count)
+	{
+		for(i = 0; i < data.row_count; i++)
+		{
+			retval = (dbh->callback)(dbh, data.row[i][1]);
+
+			if(retval)
+				delid[i] = atoi(data.row[i][0]);
+		}
+	}
+
+	rsdb_exec_fetch_end(&data);
+
+	rsdb_transaction(RSDB_TRANS_START);
+
+	for(i = 0; i < count; i++)
+	{
+		if(delid[i])
+			rsdb_exec(NULL, "DELETE FROM %s WHERE id='%u'",
+					dbh->table, delid[i]);
+	}
+
+	rsdb_transaction(RSDB_TRANS_END);
+
+	my_free(delid);
+}
