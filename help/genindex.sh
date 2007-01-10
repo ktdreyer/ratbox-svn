0bf08e73efebaf31edbd380e4c87cf8aee801fda
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2007-01-10 20:10:25 +0000

    - fix the generation of installation of helpfiles, and index generation to   accomodate languages
    git-svn-id: http://svn.ratbox.org/svnroot/rserv/trunk@23409 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- help/genindex.sh
+++ help/genindex.sh
@@ -7,36 +7,39 @@
 # If the second line is marked [ADMIN] it goes in index-admin
 # index first gets the contents of the "general" file.
 
-SUBDIRS="alis operbot chanserv userserv jupeserv operserv nickserv global banserv watchserv"
+LANGS="en_GB"
+SERVICES="alis operbot chanserv userserv jupeserv operserv nickserv global banserv watchserv"
 
-for i in $SUBDIRS; do
-	rm -f $i/index;
-	rm -f $i/index-admin;
+for k in $LANGS; do
+	for i in $SERVICES; do
+		rm -f $k/$i/index;
+		rm -f $k/$i/index-admin;
 
-	cp $i/general $i/index
+		cp $k/$i/general $k/$i/index
 
-	for j in $i/*; do
-		if [ -f $j ]; then
-			foo=`basename "$j"`;
+		for j in $k/$i/*; do
+			if [ -f $j ]; then
+				foo=`basename "$j"`;
 
-			# if it starts with "u-" then its ucommand and we
-			# dont care about it
-			case "$foo" in
-				u-*) continue;;
-				index|index-admin|general) continue;;
-			esac
+				# if it starts with "u-" then its ucommand and we
+				# dont care about it
+				case "$foo" in
+					u-*) continue;;
+					index|index-admin|general) continue;;
+				esac
 
-			arg1=`head -n 1 $j | cut -d ' ' -f 1`;
-			arg2=`head -n 2 $j | tail -n 1`;
+				arg1=`head -n 1 $j | cut -d ' ' -f 1`;
+				arg2=`head -n 2 $j | tail -n 1`;
 
-			admin=`echo "$arg2" | cut -d ' ' -f 1`;
+				admin=`echo "$arg2" | cut -d ' ' -f 1`;
 
-			if [ "$admin" = "[ADMIN]" ]; then
-				admin=`echo "$arg2" | cut -d ' ' -f 2-`;
-				printf " %-13s - %s\n" $arg1 "$admin" >> $i/index-admin;
-			else
-				printf " %-11s - %s\n" $arg1 "$arg2" >> $i/index;
+				if [ "$admin" = "[ADMIN]" ]; then
+					admin=`echo "$arg2" | cut -d ' ' -f 2-`;
+					printf " %-13s - %s\n" $arg1 "$admin" >> $k/$i/index-admin;
+				else
+					printf " %-11s - %s\n" $arg1 "$arg2" >> $k/$i/index;
+				fi
 			fi
-		fi
+		done
 	done
 done
