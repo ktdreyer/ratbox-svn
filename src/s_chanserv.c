0e52f7d3e2276f006d02b468b44a7f7723835e32
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2007-08-15 20:35:22 +0000

    - fix chanserv::info parameter ordering when issuing the notice about   suspends
    git-svn-id: http://svn.ratbox.org/svnroot/rserv/trunk@24219 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/s_chanserv.c
+++ src/s_chanserv.c
@@ -3625,12 +3625,12 @@ s_chan_info(struct client *client_p, struct lconn *conn_p, const char *parv[], i
 
 		if(config_file.cshow_suspend_reasons)
 			service_err(chanserv_p, client_p, SVC_INFO_SUSPENDEDADMIN,
-					reg_p->name, ": ",
+					reg_p->name, 
 					suspend_time ? get_short_duration(suspend_time) : "never",
-					(reg_p->suspend_reason ? reg_p->suspend_reason : ""));
+					": ", (reg_p->suspend_reason ? reg_p->suspend_reason : ""));
 		else
 			service_err(chanserv_p, client_p, SVC_INFO_SUSPENDEDADMIN,
-					reg_p->name, 
+					reg_p->name,
 					suspend_time ? get_short_duration(suspend_time) : "never",
 					"", "");
 	}
