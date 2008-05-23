57cd6703be8f43195e56f26aa8c8738516b8f694
leeh <leeh@b93f080a-4cfa-0310-a19a-80a68f531ef9>
2008-05-23 22:06:16 +0000

    - add in some funky schema generation..
    git-svn-id: http://svn.ratbox.org/svnroot/rserv/trunk@25448 b93f080a-4cfa-0310-a19a-80a68f531ef9

--- src/rsdb_schema.c
+++ src/rsdb_schema.c
@@ -10,7 +10,7 @@
 #include "rsdb.h"
 #include "rsdb_schema.h"
 
-static void rsdb_schema_generate_table(const char *name, struct rsdb_schema *schema);
+static void rsdb_schema_generate_table(const char *name, const char *primary_key, struct rsdb_schema *schema);
 
 static struct rsdb_schema rsdb_schema_users[] = 
 {
@@ -32,10 +32,125 @@ static struct rsdb_schema rsdb_schema_users[] =
 void
 rsdb_schema_generate(void)
 {
-	rsdb_schema_generate_table("users", rsdb_schema_users);
+	rsdb_schema_generate_table("users", "id", rsdb_schema_users);
 }
 
 static void
-rsdb_schema_generate_table(const char *name, struct rsdb_schema *schema)
+rsdb_schema_generate_table(const char *name, const char *primary_key, struct rsdb_schema *schema)
 {
+	dlink_list table_data;
+	static char buf[BUFSIZE];
+	int i;
+	dlink_node *ptr;
+
+	memset(&table_data, 0, sizeof(struct _dlink_list));
+
+	snprintf(buf, sizeof(buf), "CREATE TABLE %s (", name);
+	dlink_add_tail_alloc(my_strdup(buf), &table_data);
+
+	for(i = 0; schema[i].name; i++)
+	{
+		buf[0] = '\0';
+
+		switch(schema[i].option)
+		{
+			case RSDB_SCHEMA_SERIAL:
+#if defined(RSERV_DB_PGSQL)
+				snprintf(buf, sizeof(buf), "%s SERIAL, ", schema[i].name);
+#elif defined(RSERV_DB_MYSQL)
+				snprintf(buf, sizeof(buf), "%s INTEGER AUTO_INCREMENT, ", schema[i].name);
+#elif defined(RSDB_DB_SQLITE)
+				snprintf(buf, sizeof(buf), "%s INTEGER PRIMARY KEY, ", schema[i].name);
+#endif
+				break;
+
+			case RSDB_SCHEMA_BOOLEAN:
+				break;
+
+			case RSDB_SCHEMA_INT:
+				snprintf(buf, sizeof(buf), "%s INTEGER%s%s%s, ",
+					schema[i].name,
+					(schema[i].not_null ? " NOT NULL" : ""),
+					(schema[i].def != NULL ? " DEFAULT " : ""),
+					(schema[i].def != NULL ? schema[i].def : ""));
+				break;
+
+			case RSDB_SCHEMA_UINT:
+				/* no unsigned ints here */
+#if defined(RSERV_DB_PGSQL) || defined(RSERV_DB_SQLITE)
+				snprintf(buf, sizeof(buf), "%s INTEGER%s%s%s, ",
+					schema[i].name,
+					(schema[i].not_null ? " NOT NULL" : ""),
+					(schema[i].def != NULL ? " DEFAULT " : ""),
+					(schema[i].def != NULL ? schema[i].def : ""));
+#else
+				snprintf(buf, sizeof(buf), "%s INTEGER UNSIGNED%s%s%s, ",
+					schema[i].name,
+					(schema[i].not_null ? " NOT NULL" : ""),
+					(schema[i].def != NULL ? " DEFAULT " : ""),
+					(schema[i].def != NULL ? schema[i].def : ""));
+#endif
+				break;
+
+			case RSDB_SCHEMA_VARCHAR:
+#if defined(RSERV_DB_PGSQL) || defined(RSERV_DB_MYSQL)
+				snprintf(buf, sizeof(buf), "%s VARCHAR(%u)%s%s%s, ",
+					schema[i].name, schema[i].length,
+					(schema[i].not_null ? " NOT NULL" : ""),
+					(schema[i].def != NULL ? " DEFAULT " : ""),
+					(schema[i].def != NULL ? schema[i].def : ""));
+#elif defined(RSERV_DB_SQLITE)
+				snprintf(buf, sizeof(buf), "%s TEXT%s%s%s, ",
+					schema[i].name,
+					(schema[i].not_null ? " NOT NULL" : ""),
+					(schema[i].def != NULL ? " DEFAULT " : ""),
+					(schema[i].def != NULL ? schema[i].def : ""));
+#endif
+				break;
+
+			case RSDB_SCHEMA_TEXT:
+				break;
+		}
+
+		if(buf[0])
+		{
+			/* this field is the last element, either add a
+			 * primary key value, or remove the trailing ','
+			 */
+			if(schema[i+1].name == NULL)
+			{
+				/* this field has a primary key, add it now */
+				if(primary_key)
+				{
+					char tmpbuf[BUFSIZE];
+					tmpbuf[0] = '\0';
+
+#if defined(RSERV_DB_PGSQL) || defined(RSERV_DB_MYSQL)
+					snprintf(tmpbuf, sizeof(tmpbuf), "PRIMARY KEY(%s)", primary_key);
+#endif
+					strlcat(buf, tmpbuf, sizeof(buf));
+				}
+				else
+				{
+					char *x = strchr(buf, ',');
+					if(x)
+						*x = '\0';
+				}
+			}
+
+			dlink_add_tail_alloc(my_strdup(buf), &table_data);
+		}
+	}
+
+	snprintf(buf, sizeof(buf), ");");
+	dlink_add_tail_alloc(my_strdup(buf), &table_data);
+
+#if 1
+	DLINK_FOREACH(ptr, table_data.head)
+	{
+		fprintf(stderr, "%s", (const char *) ptr->data);
+	}
+
+	fprintf(stderr, "\n");
+#endif
 }
